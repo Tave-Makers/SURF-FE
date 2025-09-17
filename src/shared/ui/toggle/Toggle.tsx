f1a3d03765d4acd35fc62f7ec26ca18aac595703
jinj00oo <icems0428@naver.com>
2025-09-17 16:31:33 +0900

    [refactor][jinj00oo]: className 및 불필요한 속성 props에서 제외 (#14)
    - Controlled와 UnControlled 전용 타입을 분리하여 컴파일 단계에서 잘못된 사용 차단

--- src/shared/ui/toggle/Toggle.tsx
+++ src/shared/ui/toggle/Toggle.tsx
@@ -1,23 +1,41 @@
 'use client';
 
-import { forwardRef, InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes, ChangeEvent } from 'react';
 
-export type ToggleProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
+// 공통 속성
+type BaseProps = Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'type' | 'children' | 'checked' | 'defaultChecked' | 'disabled' | 'onChange' | 'className'
+> & {
   id: string; // 고유 ID
   label?: string; // 옆에 표시할 텍스트
-  isChecked?: boolean; // Controlled 모드
-  isDefaultChecked?: boolean; // Uncontrolled 모드 초기값
-  isDisabled?: boolean; // 비활성화 여부
-  name?: string; // 폼 name
-  value?: string; // 폼 value
+  isDisabled?: boolean;
+  className?: string; // wrapper 확장용 (필요 없으면 제거)
 };
+
+// Controlled 전용
+type ControlledProps = {
+  isChecked: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  isDefaultChecked?: never;
+};
+
+// Uncontrolled 전용
+type UncontrolledProps = {
+  isDefaultChecked?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  isChecked?: never;
+};
+
+// 최종 Props
+export type ToggleProps = BaseProps & (ControlledProps | UncontrolledProps);
 
 export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
   ({ id, label, isChecked, isDefaultChecked, isDisabled, name, value, ...rest }, ref) => {
     return (
       <label
         htmlFor={id}
-        className={`inline-flex items-center gap-[0.625rem] ${
+        className={`inline-flex items-center gap-[0.62rem] ${
           isDisabled ? '!cursor-not-allowed' : 'cursor-pointer'
         }`}
       >
@@ -34,9 +52,9 @@ export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
           className="peer sr-only"
           {...rest}
         />
-        <div className="peer-checked:bg-background-primary bg-background-hint flex h-[1.125rem] w-[1.875rem] items-center rounded-full p-[0.09375rem] transition-colors duration-300 peer-checked:justify-end">
+        <div className="peer-checked:bg-background-primary bg-background-hint flex h-[1.12rem] w-[1.87rem] items-center rounded-full p-[0.09rem] transition-colors duration-300 peer-checked:justify-end">
           <span
-            className={`h-[0.9375rem] w-[0.9375rem] rounded-full transition-transform duration-300 ${
+            className={`h-[0.93rem] w-[0.93rem] rounded-full transition-transform duration-300 ${
               isDisabled ? 'bg-foreground-hint-darker' : 'bg-foreground-accent'
             }`}
           />

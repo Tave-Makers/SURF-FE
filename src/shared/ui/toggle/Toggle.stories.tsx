95d96612f532c0652805841d8c8911157f656067
jinj00oo <icems0428@naver.com>
2025-09-17 16:27:55 +0900

    [chore][jinj00oo]: 디렉토리 경로 수정 (#14)

--- src/shared/ui/toggle/Toggle.stories.tsx
+++ src/shared/ui/toggle/Toggle.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { Toggle } from './Toggle';
 
 const meta: Meta<typeof Toggle> = {
-  title: 'shared/ui/Toggle',
+  title: 'Shared/UI/Toggle',
   component: Toggle,
   tags: ['autodocs'],
   args: {

b806daa138993dd658bc165c49673dbdafbe93aa
jinj00oo <icems0428@naver.com>
2025-09-18 01:33:13 +0900

    [chore][jinj00oo]: Chip 컴포넌트 높이 지정 (#15)

--- src/shared/ui/chip/Chip.tsx
+++ src/shared/ui/chip/Chip.tsx
@@ -4,7 +4,7 @@ type ChipProps = {
 
 export const Chip = ({ children }: ChipProps) => {
   return (
-    <span className="font-pretendard bg-background-hint text-foreground-accent inline-block rounded-[0.12rem] px-[0.37rem] py-[0.25rem] text-center text-[0.625rem] leading-[0.6875rem] font-semibold tracking-[-0.00625rem] [font-feature-settings:'liga_off','clig_off']">
+    <span className="font-pretendard bg-background-hint text-foreground-accent inline-block h-[1.18rem] rounded-[0.12rem] px-[0.37rem] py-[0.25rem] text-center text-[0.625rem] leading-[0.6875rem] font-semibold tracking-[-0.00625rem] [font-feature-settings:'liga_off','clig_off']">
       {children}
     </span>
   );

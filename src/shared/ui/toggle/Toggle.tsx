d508ef54a90f61f03c808d52a9f520f71bebf1d9
jinj00oo <icems0428@naver.com>
2025-09-20 01:16:36 +0900

    [chore][jinj00oo]: 토글 핸들 transform 애니메이션 적용 (#15)
    - peer -> group 방식으로 바꾸어 깊은 하위 자식까지 상태 전파

--- src/shared/ui/toggle/Toggle.tsx
+++ src/shared/ui/toggle/Toggle.tsx
@@ -2,32 +2,28 @@
 
 import { forwardRef, InputHTMLAttributes, ChangeEvent, useId } from 'react';
 
-// 공통 속성
 type BaseProps = Omit<
   InputHTMLAttributes<HTMLInputElement>,
   'type' | 'children' | 'checked' | 'defaultChecked' | 'disabled' | 'onChange' | 'className'
 > & {
-  id?: string; // 고유 ID
-  label?: string; // 옆에 표시할 텍스트
+  id?: string;
+  label?: string;
   isDisabled?: boolean;
-  className?: string; // wrapper 확장용 (필요 없으면 제거)
+  className?: string;
 };
 
-// Controlled 전용
 type ControlledProps = {
   isChecked: boolean;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   isDefaultChecked?: never;
 };
 
-// Uncontrolled 전용
 type UncontrolledProps = {
   isDefaultChecked?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   isChecked?: never;
 };
 
-// 최종 Props
 export type ToggleProps = BaseProps & (ControlledProps | UncontrolledProps);
 
 export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
@@ -40,7 +36,7 @@ export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
         htmlFor={inputId}
         className={`inline-flex w-fit items-center gap-[0.62rem] ${
           isDisabled ? '!cursor-not-allowed' : 'cursor-pointer'
-        }`}
+        } group`}
       >
         <input
           id={inputId}
@@ -52,14 +48,16 @@ export const Toggle = forwardRef<HTMLInputElement, ToggleProps>(
           defaultChecked={isDefaultChecked}
           disabled={isDisabled}
           aria-label={label ? undefined : '토글 버튼'}
-          className="peer sr-only"
+          className="sr-only"
           {...rest}
         />
-        <div className="peer-checked:bg-background-primary bg-background-hint flex h-[1.12rem] w-[1.87rem] items-center rounded-full p-[0.09rem] transition-colors duration-300 peer-checked:justify-end">
+        {/* 토글 트랙 */}
+        <div className="bg-background-hint group-has-[:checked]:bg-background-primary relative h-[1.125rem] w-[1.875rem] rounded-full transition-colors duration-300 group-has-[:focus-visible]:outline-[1.5px] group-has-[:focus-visible]:outline-offset-2">
+          {/* 토글 핸들 */}
           <span
-            className={`h-[0.93rem] w-[0.93rem] rounded-full transition-transform duration-300 ${
+            className={`absolute top-[0.09rem] left-[0.09rem] h-[0.9375rem] w-[0.9375rem] rounded-full transition-transform duration-300 will-change-transform ${
               isDisabled ? 'bg-foreground-hint-darker' : 'bg-foreground-accent'
-            }`}
+            } group-has-[:checked]:translate-x-[0.76rem]`}
           />
         </div>
         {label && <span className="text-caption-12-400 text-border-contrast">{label}</span>}

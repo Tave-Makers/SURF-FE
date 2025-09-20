e098add5c8f4a73957a3b8aaa31b823a1659674d
Sungmoon Yoon <xhdrp@sookmyung.ac.kr>
2025-09-20 15:11:02 +0900

    [feat][chaejy]: 스피너 컴포넌트 제작

---
+++ src/shared/ui/spinner/Spinner.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { Spinner } from './Spinner';
+
+const meta: Meta<typeof Spinner> = {
+  title: 'Shared/Spinner',
+  component: Spinner,
+};
+
+export default meta;
+type Story = StoryObj<typeof Spinner>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/shared/ui/spinner/Spinner.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Wheel } from './Wheel';
+import './styles.css';
+
+const parts = ['데이터분석', '디자인', '딥러닝', '백엔드', '앱 프론트엔드', '웹 프론트엔드'];
+const periods = [
+  '1기',
+  '2기',
+  '3기',
+  '4기',
+  '5기',
+  '6기',
+  '7기',
+  '8기',
+  '9기',
+  '10기',
+  '11기',
+  '12기',
+  '13기',
+  '14기',
+  '15기',
+  '16기',
+  '17기',
+  '18기',
+  '19기',
+  '20기',
+];
+
+export const Spinner = () => {
+  function formateParts(_relative: number, absolute: number) {
+    return parts[absolute];
+  }
+
+  function formatePeriods(_relative: number, absolute: number) {
+    return periods[absolute];
+  }
+
+  return (
+    <div
+      style={{
+        width: '335px',
+        height: '176px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: '#fff',
+      }}
+    >
+      <div style={{ width: 155, height: 160 }}>
+        <Wheel initIdx={1} length={parts.length} width={150} loop={false} setValue={formateParts} />
+      </div>
+
+      <div style={{ width: 155, height: 160 }}>
+        <Wheel
+          initIdx={1}
+          length={periods.length}
+          width={150}
+          loop={false}
+          perspective="left"
+          setValue={formatePeriods}
+        />
+      </div>
+    </div>
+  );
+};

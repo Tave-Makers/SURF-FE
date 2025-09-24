5aca423d63e630f4e24ae3bf18c3c88de58818d5
rngrhn4114 <is9206855@gmail.com>
2025-09-24 21:59:03 +0900

    [feat][rngrhn4114]: 온보딩 첫단계(프로필 생성)화면 라우팅 추가

---
+++ app/(sub)/onboarding/create-profile/page.tsx
@@ -0,0 +1,4 @@
+import CreateProfilePage from '@/app-pages/onboarding-create-profile/ui/CreateProfilePage';
+export default function Page() {
+  return <CreateProfilePage />;
+}
---
+++ src/app-pages/onboarding-create-profile/ui/CreateProfilePage.tsx
@@ -0,0 +1,12 @@
+// import { Header } from '@/shared/ui/Header';
+import { ProfileForm } from '@/widgets/onbarding/ProfileForm';
+
+export default function CreateProfilePage() {
+  return (
+    <div>
+      <div>헤더 컴포넌트</div>
+      {/* // <Header /> */}
+      <ProfileForm />
+    </div>
+  );
+}

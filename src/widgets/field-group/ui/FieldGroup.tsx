3981e16c4f9ffa547d67c9af1fda0d32d7f30cda
Jiyeon <144954836+Chaejy@users.noreply.github.com>
2025-09-26 22:23:40 +0900

    [fix][chaejy]: 필수사항 기본값 false로 지정

--- src/widgets/field-group/ui/FieldGroup.tsx
+++ src/widgets/field-group/ui/FieldGroup.tsx
@@ -9,7 +9,7 @@ type FieldGroupProps = {
   className?: string;
 };
 
-export const FieldGroup = ({ title, isRequired, children, className = '' }: FieldGroupProps) => {
+export const FieldGroup = ({ title, isRequired = false, children, className = '' }: FieldGroupProps) => {
   return (
     <fieldset className={`m-0 flex flex-col gap-[0.625rem] border-0 p-0 ${className}`} role="group">
       <legend className="text-body-16-600--1 text-foreground-normal">

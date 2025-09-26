c51f1898072f43424038264454fc0268b62c153c
DaheeJang <jecydahee77@gmail.com>
2025-09-27 06:31:33 +0900

    [fix][jecy77]: Suspense 사용으로 useSearchParams가 CSR에서만 안전하게 동작 및 빌드에러 해결 (#62)

--- app/login/callback/page.tsx
+++ app/login/callback/page.tsx
@@ -1,5 +1,11 @@
 import { KakaoCallBackPage } from '@/app-pages/login/ui/KakaoCallBackPage';
+import { Suspense } from 'react';
 
 export default function Page() {
-  return <KakaoCallBackPage />;
+  return (
+    // fallback은 추후 로딩 중 화면으로 대체
+    <Suspense fallback={<div>로딩중...</div>}>
+      <KakaoCallBackPage />
+    </Suspense>
+  );
 }

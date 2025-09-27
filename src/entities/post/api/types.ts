1d969da0b49fd831394438a86bf2c9dfe7206a83
rngrhn4114 <is9206855@gmail.com>
2025-09-28 01:40:33 +0900

    [feat][rngrhn4114]: 게시물 응답 및 요청 타입 정의

---
+++ src/entities/post/api/types.ts
@@ -0,0 +1,50 @@
+// 응답 Sort
+export type PostSort = {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+};
+
+// 페이지네이션
+export type Pageable = {
+  offset: number;
+  sort: PostSort;
+  paged: boolean;
+  pageNumber: number;
+  pageSize: number;
+  unpaged: boolean;
+};
+
+// 게시물 타입
+export type PostContent = {
+  id: number;
+  title: string;
+  content: string;
+  pinned: boolean;
+  postedAt: Date;
+  boardId: number;
+  scrappedByMe: boolean;
+  scrapCount: number;
+};
+
+// 게시물 관련 API 응답 전체 타입
+export type PostApiResponse = {
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  content: PostContent[];
+  number: number;
+  sort: PostSort;
+  pageable: Pageable;
+  first: boolean;
+  last: boolean;
+  numberOfElements: number;
+  empty: boolean;
+};
+
+// 게시물 API 요청 타입
+export type PostApiRequest = {
+  page: number;
+  size: number;
+  sort: string[];
+};

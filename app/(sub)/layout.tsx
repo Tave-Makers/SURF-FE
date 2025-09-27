48d89e511927851363bb2ac183b1327b83323905
Dahee Jang <122618383+jecy77@users.noreply.github.com>
2025-09-27 19:48:15 +0900

    [FEAT] 카카오로그인 구현 (#67)
    * [chore][jecy77]: router.back()을 기본 적용, 하위 경로를 위한 route 설정 추가
    
    * [feat][jecy77]: 카카오 로그인 구현 (#62)
    
    * [fix][jecy77]: 콜백 경로를 히스토리에 남기지 않도록 하여 인가코드 재사용 문제 해결 (#62)
    
    * [fix][jecy77]: Suspense 사용으로 useSearchParams가 CSR에서만 안전하게 동작 및 빌드에러 해결 (#62)

--- app/(sub)/layout.tsx
+++ app/(sub)/layout.tsx
@@ -4,7 +4,7 @@ export default function SubLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-full w-full flex-col">
       <AppHeader />
-      <section className="flex-1 px-[1rem]">{children}</section>
+      <section className="flex-1">{children}</section>
     </div>
   );
 }

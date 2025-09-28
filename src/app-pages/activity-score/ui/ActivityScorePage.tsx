84266adafb934d1b98332da1ab270acec3f8d024
jinj00oo <icems0428@naver.com>
2025-09-28 17:58:04 +0900

    [chore][jinj00oo]: 활동점수 요약 api 연동 (#71)
    - memberId는 임시 하드코딩

--- src/app-pages/activity-score/ui/ActivityScorePage.tsx
+++ src/app-pages/activity-score/ui/ActivityScorePage.tsx
@@ -1,48 +1,45 @@
 'use client';
 
-import {
-  ActivitySummaryRecords,
-  ScoreMode,
-  ActivityHistory,
-} from '@/entities/activity-score/model/types';
+import { ScoreMode, ActivityHistory } from '@/entities/activity-score/model/types';
 import { useState } from 'react';
 import ActivityScoreCard from '@/widgets/activity-score/ActivityScoreCard';
 import { ActivityHistoryList } from '@/entities/activity-score/ui/ActivityHistoryList';
 import { Tab } from '@/shared/ui/tab/Tab';
+import { useActivitySummary } from '@/entities/activity-score/model/useActivitySummary';
 
 // TODO: API 응답으로 변경
-const mockData: ActivitySummaryRecords = {
-  rewards: {
-    taveActivities: [
-      { activityType: 'UPLOAD_INSTAGRAM_STORY', count: 7 },
-      { activityType: 'ENGAGE_TECH_SEMINAR', count: 0 },
-      { activityType: 'EARLY_BIRD', count: 4 },
-    ],
-    blogs: {
-      totalCount: 2,
-      list: [
-        { activityType: 'WRITE_WIL', count: 1 },
-        { activityType: 'UPLOAD_TAVE_REVIEW', count: 1 },
-      ],
-    },
-  },
-  penalties: {
-    late: {
-      totalCount: 9,
-      list: [
-        { activityType: 'SESSION_LATE', count: 3 },
-        { activityType: 'TEAM_LATE', count: 6 },
-      ],
-    },
-    absence: {
-      totalCount: 4,
-      list: [
-        { activityType: 'SESSION_ABSENCE', count: 2 },
-        { activityType: 'TEAM_ABSENCE', count: 2 },
-      ],
-    },
-  },
-};
+// const mockData: ActivitySummaryRecords = {
+//   rewards: {
+//     taveActivities: [
+//       { activityType: 'UPLOAD_INSTAGRAM_STORY', count: 7 },
+//       { activityType: 'ENGAGE_TECH_SEMINAR', count: 0 },
+//       { activityType: 'EARLY_BIRD', count: 4 },
+//     ],
+//     blogs: {
+//       totalCount: 2,
+//       list: [
+//         { activityType: 'WRITE_WIL', count: 1 },
+//         { activityType: 'UPLOAD_TAVE_REVIEW', count: 1 },
+//       ],
+//     },
+//   },
+//   penalties: {
+//     late: {
+//       totalCount: 9,
+//       list: [
+//         { activityType: 'SESSION_LATE', count: 3 },
+//         { activityType: 'TEAM_LATE', count: 6 },
+//       ],
+//     },
+//     absence: {
+//       totalCount: 4,
+//       list: [
+//         { activityType: 'SESSION_ABSENCE', count: 2 },
+//         { activityType: 'TEAM_ABSENCE', count: 2 },
+//       ],
+//     },
+//   },
+// };
 
 export const mockRewardRecords: ActivityHistory[] = [
   { memberId: 1, date: '26.09.06', category: '얼리버드', delta: 10, total: 136 },
@@ -56,6 +53,11 @@ export const mockPenaltyRecords: ActivityHistory[] = [
 
 export default function ActivityScorePage() {
   const [mode, setMode] = useState<ScoreMode>('REWARD');
+  const {
+    data: summary,
+    isLoading: isSummaryLoading,
+    isError: isSummaryError,
+  } = useActivitySummary(1); // TODO: memberId 실제 값으로 교체
 
   const records = mode === 'REWARD' ? mockRewardRecords : mockPenaltyRecords;
 
@@ -63,7 +65,11 @@ export default function ActivityScorePage() {
     <div className="flex flex-col items-center">
       {/* 활동 점수 카드 */}
       <div className="pt-[1.88rem] pb-[2.5rem]">
-        <ActivityScoreCard score={156} records={mockData} mode={mode} />
+        {isSummaryLoading && <div>불러오는 중...</div>}
+        {isSummaryError && <div>데이터를 불러오는 중 오류가 발생했습니다.</div>}
+        {summary && (
+          <ActivityScoreCard score={summary.score} records={summary.records} mode={mode} />
+        )}
       </div>
 
       {/* 탭 버튼 */}

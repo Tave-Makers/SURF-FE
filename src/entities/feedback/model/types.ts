915be8537f482a5f793f61e8a978bce4f1159a34
rngrhn4114 <is9206855@gmail.com>
2025-09-28 14:53:33 +0900

    [feat][rngrhn4114]: 피드백 보내기 기능 타입 정의

--- src/entities/feedback/model/types.ts
+++ src/entities/feedback/model/types.ts
@@ -1,5 +1,11 @@
-export type Feedback = {
+// 피드백 보내기 API 응답 타입
+export type SubmitFeedBackApiResponse = {
   id: string;
   content: string;
   date: string;
+};
+
+// 피드백 보내기 API 요청 타입
+export type SubmitFeedBackApiRequest = {
+  content: string;
 };

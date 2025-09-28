1a7d895de7fe43ec790a6f88975dc8ab490e8644
rngrhn4114 <is9206855@gmail.com>
2025-09-28 17:21:51 +0900

    [fix][rngrhn4114]: providers 최상위 layout으로 이동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
 import type { ReactNode } from 'react';
 import '@/shared/styles/globals.css';
+import { QueryProvider } from '@/app/providers/QueryProvider';
+import { AuthProvider } from '@/app/providers/AuthProvider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="ko">
-      <head>
-        <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin="anonymous"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
-        />
-        <link
-          href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square-neo.css"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="flex min-h-screen items-center justify-center bg-gray-100">
-        <main className="bg-background-normal h-dvh w-dvw sm:w-[360px]">{children}</main>
-      </body>
-    </html>
+    <QueryProvider>
+      <AuthProvider>
+        <html lang="ko">
+          <head>
+            <link
+              rel="stylesheet"
+              as="style"
+              crossOrigin="anonymous"
+              href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
+            />
+            <link
+              href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square-neo.css"
+              rel="stylesheet"
+            />
+          </head>
+          <body className="flex min-h-screen items-center justify-center bg-gray-100">
+            <main className="bg-background-normal h-dvh w-dvw sm:w-[360px]">{children}</main>
+          </body>
+        </html>
+      </AuthProvider>
+    </QueryProvider>
   );
 }

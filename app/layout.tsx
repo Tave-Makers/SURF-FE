a9298591ad19e4c4ea6f9ae48097a5aa6b243558
jinj00oo <icems0428@naver.com>
2025-09-28 17:51:45 +0900

    [refactor][jinj00oo]: provider 루트 레이아웃으로 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from 'react';
 import '@/shared/styles/globals.css';
+import { AuthProvider } from '@/app/providers/AuthProvider';
+import { QueryProvider } from '@/app/providers/QueryProvider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -17,7 +19,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
       </head>
       <body className="flex min-h-screen items-center justify-center bg-gray-100">
-        <main className="bg-background-normal h-dvh w-dvw sm:w-[360px]">{children}</main>
+        <QueryProvider>
+          <AuthProvider>
+            <main className="bg-background-normal h-dvh w-dvw sm:w-[360px]">{children}</main>
+          </AuthProvider>
+        </QueryProvider>
       </body>
     </html>
   );

fd31b72a75ff1530e6b902fe6240dd5cd09e7c50
rngrhn4114 <is9206855@gmail.com>
2025-09-28 13:52:31 +0900

    [fix][rngrhn411]: 약관 라우팅 경로 수정

--- src/app-pages/mypage-policy/ui/PolicyPage.tsx
+++ src/app-pages/mypage-policy/ui/PolicyPage.tsx
@@ -11,7 +11,7 @@ export default function PolicyPage() {
       <PolicyItem
         rightIconName="ChevronRight"
         onClick={() => {
-          router.push('/mypage/policy/service-policy');
+          router.push('/mypage/settings/policy/service-policy');
         }}
       >
         [필수] SURF 이용약관
@@ -19,7 +19,7 @@ export default function PolicyPage() {
       <PolicyItem
         rightIconName="ChevronRight"
         onClick={() => {
-          router.push('/mypage/policy/personal-info-policy');
+          router.push('/mypage/settings/policy/personal-info-policy');
         }}
       >
         [필수] 개인정보 수집·이용 동의서
@@ -27,7 +27,7 @@ export default function PolicyPage() {
       <PolicyItem
         rightIconName="ChevronRight"
         onClick={() => {
-          router.push('/mypage/policy/marketing-info-policy');
+          router.push('/mypage/settings/policy/marketing-info-policy');
         }}
       >
         [선택] 마케팅 정보 수신 동의

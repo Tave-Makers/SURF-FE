8051cd2c00b95287c64d33da2dd3c3a4d39d5080
rngrhn4114 <is9206855@gmail.com>
2025-09-28 15:38:28 +0900

    [fix][rngrhn4114]: 배포 에러- 파일 경로 문제 해결

--- src/app-pages/mypage-scraps/ui/ScrapsPage.tsx
+++ src/app-pages/mypage-scraps/ui/ScrapsPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { PostList } from '@/widgets/post-list';
+import { PostList } from '@/widgets/post-list/ui/PostList';
 // import { useState } from 'react';
 
 export default function ScrapsPage() {

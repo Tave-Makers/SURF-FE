e0d2007f3b2db89db8d59bf683ca2bb0a2122a9a
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-09-30 18:45:15 +0900

    [fix][chaejy]: vercel 페이지 없어서 배포 실패한 오류 수정

--- src/app-pages/mypage-edit/ui/MyEditPage.tsx
+++ src/app-pages/mypage-edit/ui/MyEditPage.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { useState } from 'react';
-import { ProfileEditHeader } from '@/widgets/profile-edit-header/ui/ProfileEditHeader';
-import { ProfileEditForm } from '@/widgets/profile-edit-form/ui/ProfileEditForm';
+//import { useState } from 'react';
+//import { ProfileEditHeader } from '@/widgets/profile-edit-header/ui/ProfileEditHeader';
+//import { ProfileEditForm } from '@/widgets/profile-edit-form/ui/ProfileEditForm';
 import { TextButton } from '@/shared/ui/text-button/TextButton';
 
 export function MyEditPage() {
-  const [memberName] = useState('김테이비');
+  //const [memberName] = useState('김테이비');
 
   const handleEdit = () => {
     // TODO: 편집 저장/전환 로직 또는 라우팅 연결
@@ -18,8 +18,6 @@ export function MyEditPage() {
 
   return (
     <div className="flex flex-col">
-      <ProfileEditHeader memberName={memberName} />
-      <ProfileEditForm />
       <div className="flex w-full flex-col gap-[0.5rem] px-[1rem] py-[0.62rem]">
         <div className="flex w-[4rem] self-end">
           <TextButton size="s" variant="secondary" onClick={handleEdit}>

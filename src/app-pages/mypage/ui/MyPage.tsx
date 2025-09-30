fb17ab14eacbd35989611a8c0b6380b114c504be
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-09-30 16:20:11 +0900

    [feat][chaejy]: 마이페이지 ui 구현

--- src/app-pages/mypage/ui/MyPage.tsx
+++ src/app-pages/mypage/ui/MyPage.tsx
@@ -1,7 +1,21 @@
-export function MyPage() {
+import { UserLevel } from '@/features/profile/model/userLevel';
+import { MyPageActions } from '@/widgets/mypage-actions/ui/MyPageActions';
+import { ProfileTabs } from '@/widgets/profile-tabs/ui/ProfileTabs';
+import { ProfileHeader } from '@/widgets/profile-header/ui/ProfileHeader';
+
+type MyPageProps = {
+  name: string;
+  level: UserLevel;
+  chips: string[];
+  isActiveMember: boolean;
+};
+
+export function MyPage({ name, level, chips, isActiveMember }: MyPageProps) {
   return (
-    <>
-      <div>mypage</div>
-    </>
+    <div className="flex flex-col">
+      <ProfileHeader name={name} level={level} chips={chips} />
+      <MyPageActions isActiveMember={isActiveMember} />
+      <ProfileTabs />
+    </div>
   );
 }

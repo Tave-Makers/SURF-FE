f216b55e563a5de4b074b40ccae1eb5bf4004269
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-09-30 15:46:07 +0900

    [feat][chaejy]: 마이페이지 프로필 전환 탭 제작

---
+++ src/widgets/profile-tabs/ui/ProfileTabs.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useState } from 'react';
+import { Tab } from '@/shared/ui/tab/Tab';
+import { FieldGroup } from '@/shared/ui/field-group/FieldGroup';
+import { TextArea } from '@/shared/ui/text-area/TextArea';
+import { ActivityBadge } from '@/shared/ui/activity-badge/ActivityBadge';
+
+export function ProfileTabs() {
+  const [tab, setTab] = useState('profile');
+
+  return (
+    <>
+      <Tab
+        items={[
+          { value: 'profile', label: '프로필' },
+          { value: 'badges', label: '활동뱃지' },
+        ]}
+        value={tab}
+        onValueChange={setTab}
+      />
+      <div className="flex flex-col">
+        {tab === 'profile' ? (
+          <div className="flex flex-col gap-[1.5rem] px-[1rem] py-[1.25rem]">
+            <FieldGroup title="전화번호" isRequired>
+              <TextArea value="010-1234-5678" onChange={() => {}} />
+            </FieldGroup>
+            <FieldGroup title="이메일" isRequired>
+              <TextArea value="test@test.com" onChange={() => {}} />
+            </FieldGroup>
+            <FieldGroup title="학교" isRequired>
+              <TextArea value="테이브대학교" onChange={() => {}} />
+              <TextArea value="테이브대학교 대학원" onChange={() => {}} />
+            </FieldGroup>
+            <FieldGroup title="경력" isRequired>
+              <TextArea value="테이브 개발자" onChange={() => {}} />
+            </FieldGroup>
+          </div>
+        ) : (
+          <div className="justify-space-between flex flex-col items-start gap-[3rem] p-[2.5rem]">
+            <ActivityBadge badgeName="출석왕" timestamp="2025-09-18" />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}

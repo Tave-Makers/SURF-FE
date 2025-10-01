9989200c5ba54a9ffb742871c09169884195ae95
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-01 10:23:20 +0900

    [feat][chaejy]: 마이페이지 라우팅

--- app/(main)/mypage/page.tsx
+++ app/(main)/mypage/page.tsx
@@ -1,11 +1,4 @@
 import { MyPage } from '@/app-pages/mypage/ui/MyPage';
 export default function Page() {
-  return (
-    <MyPage
-      name="김테이비"
-      level="manager"
-      chips={['13기 백엔드', '13기 프론트엔드']}
-      isActiveMember={true}
-    />
-  );
+  return <MyPage />;
 }

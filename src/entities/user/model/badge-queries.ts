132eb28eb4d2dc5ec08c52c5fccb7b4803980134
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-01 10:04:28 +0900

    [feat][chaejy]: 활동뱃지 무한스크롤  훅

---
+++ src/entities/user/model/badge-queries.ts
@@ -0,0 +1,23 @@
+'use client';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getMemberBadges } from '@/entities/user/api/index';
+
+export const badgeKeys = {
+  all: (memberId: number) => ['badges', memberId] as const,
+  my: () => ['badges', 'my'] as const,
+};
+
+export function useBadgesInfiniteQuery(memberId?: number, pageSize = 9) {
+  return useInfiniteQuery({
+    queryKey: memberId ? badgeKeys.all(memberId) : badgeKeys.my(),
+    initialPageParam: 0,
+    queryFn: ({ pageParam }) =>
+      getMemberBadges({
+        ...(memberId && { memberId }),
+        pageNum: pageParam,
+        pageSize,
+      }),
+    getNextPageParam: (last) => (last.isLast ? undefined : last.pageNumber + 1),
+  });
+}

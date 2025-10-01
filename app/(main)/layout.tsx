0536062c38113f45d1c32a582c994d7c927c6dfc
rngrhn4114 <is9206855@gmail.com>
2025-10-02 00:20:09 +0900

    [chore][rngrhn4114]: 하위 layout에서 provider 삭제

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,18 +1,12 @@
-import { AuthProvider } from '@/app/providers/AuthProvider';
-import { QueryProvider } from '@/app/providers/QueryProvider';
 import { AppHeader } from '@/widgets/header/ui/AppHeader';
 import { AppNavigation } from '@/widgets/navigation/ui/AppNavigation';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <QueryProvider>
-      <AuthProvider>
-        <div className="flex h-full w-full flex-col">
-          <AppHeader />
-          <section className="flex-1 overflow-auto">{children}</section>
-          <AppNavigation />
-        </div>
-      </AuthProvider>
-    </QueryProvider>
+    <div className="flex h-full w-full flex-col">
+      <AppHeader />
+      <section className="flex-1 overflow-auto">{children}</section>
+      <AppNavigation />
+    </div>
   );
 }
--- app/(sub)/layout.tsx
+++ app/(sub)/layout.tsx
@@ -1,13 +1,10 @@
 import { AppHeader } from '@/widgets/header/ui/AppHeader';
-import { QueryProvider } from '@/app/providers/QueryProvider';
 
 export default function SubLayout({ children }: { children: React.ReactNode }) {
   return (
-    <QueryProvider>
-      <div className="flex h-full w-full flex-col">
-        <AppHeader />
-        <section className="h-full w-full flex-1 overflow-auto">{children}</section>
-      </div>
-    </QueryProvider>
+    <div className="flex h-full w-full flex-col">
+      <AppHeader />
+      <section className="h-full w-full flex-1 overflow-auto">{children}</section>
+    </div>
   );
 }

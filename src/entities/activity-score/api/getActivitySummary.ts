8e1671a0cb1350a2fbc8645ed2d0b087fcba6130
jinj00oo <157488644+jinj00oo@users.noreply.github.com>
2025-10-03 20:27:29 +0900

    [FEAT] 활동점수 요약 api 연동 (#80)
    * [chore][jinj00oo]: 폴더 구조 레이아웃에 따른 정리
    
    * [chore][jinj00oo]: 활동점수 아이콘 저장 (#52)
    
    * [chore][jinj00oo]: zustand 설치 (#52)
    
    - 툴팁 전역 상태 관리를 위함
    
    * [feat][jinj00oo]: 활동점수 관련 모델 추가 (#52)
    
    - meta 데이터
    - 툴팁 상태
    - 타입 정의
    
    * [feat][jinj00oo]: 활동점수 관련 UI 컴포넌트 추가 (#52)
    
    * [feat][jinj00oo]: 활동점수 페이지/라우팅 추가 (#52)
    
    * [fix][chaejy]: 폰트 토큰 수정 (#57)
    
    * [feat][chaejy]: 탭 컴포넌트 제작 (#56)
    
    * [fix][chaejy]: 탭에 패딩 추가
    
    * [feat][chaejy]: 나눔스퀘어 네오 웹폰트 추가
    
    * [feat][chaejy]: 전역 버튼, a태그에 커서 포인터 설정
    
    * [fix][chaejy]: 폰트 네이밍 수정
    
    * [fix][chaejy]: border 수정
    
    * [chore][jinj00oo]: 지각, 결석 아이콘 저장 (#52)
    
    * [feat][jinj00oo]: 활동 점수 카드에 벌점 목록 추가 (#52)
    
    - 탭 상태에 따른 상벌점 전환
    
    * [chore][jinj00oo]: 페이지에 변경된 활동 점수 카드 반영 (#52)
    
    * [fix][jinj00oo]: 오타 수정
    
    * [chore][jinj00oo]: 불필요한 label 제거
    
    * [refactor][jinj00oo]: 상수화를 통한 키 유니온 보존 (#52)
    
    * [feat][jinj00oo]: 활동 점수 히스토리용 타입 추가 (#52)
    
    - 기존 카드용 타입들과 명확한 구분을 위하여 네이밍 리팩토링
    
    * [chore][jinj00oo]: 라벨명 수정 (#52)
    
    * [chore][jinj00oo]: 파일명 수정 (#52)
    
    - 활동 점수 카드 관련 컴포넌트명은 Summary를 사용하여 활동 기록 리스트 컴포넌트와 구분
    
    * [refactor][jinj00oo]: ActivityScoreCard 리팩토링 (#52)
    
    * [chore][jinj00oo]: 백엔드 API 응답 가공 함수 제작 (#52)
    
    - 응답 필드명 이해하기 쉽도록 수정
    - `activity`가 `null`일 때 optional 처리
    
    * [feat][jinj00oo]: 활동 기록 관련 컴포넌트 제작 (#52)
    
    * [feat][jinj00oo]: 활동 점수 페이지에 활동 기록 컴포넌트 추가 (#52)
    
    - 탭 컴포넌트는 임시 처리
    
    * [feat][jinj00oo]: 활동 점수 페이지에 활동 기록 컴포넌트 추가 (#52)
    
    - 탭 컴포넌트는 임시 처리
    
    * [chore][jinj00oo]: 레이아웃 구조 원래대로 되돌리기
    
    - 충돌 해결 위함
    
    * [feat][jinj00oo]: 활동점수 페이지 경로 수정 (#52)
    
    * [fix][chaejy]: value로 controlled/uncontrolled 구분
    
    * [chore][jinj00oo]: ts 파일로 변경
    
    * [chore][jinj00oo]: Icon값 null로 변경하여 의도적 빈 값 명시
    
    * [chore][jinj00oo]: 넥스트 타이머 타입 일치
    
    * [chore][jinj00oo]: 임시탭 탭 컴포넌트로 교체
    
    * [chore][jinj00oo]: 레이아웃 및 탭 컴포넌트 스타일링 수정
    
    * [chore][jinj00oo]: tanstackQuery 설치 (#71)
    
    * [refactor][jinj00oo]: provider 루트 레이아웃으로 분리
    
    * [refactor][jinj00oo]: 공통 타입 분리
    
    * [feat][jinj00oo]: 활동점수 요약 api 함수 작성 (#71)
    
    * [feat][jinj00oo]: 활동점수 요약 tanstackQuery 훅 작성 (#71)
    
    * [chore][jinj00oo]: 활동점수 요약 api 연동 (#71)
    
    - memberId는 임시 하드코딩
    
    * [chore][jinj00oo]: 앱헤더 코드 수정
    
    * [chore][jinj00oo]: memberId 삭제 (#71)
    
    * [chore][jinj00oo]: tastackQuery devtools 설치
    
    * [chore][jinj00oo]: devtools layout 추가
    
    * [chore][jinj00oo]: 타입 설명 주석 추가
    
    * [chore][jinj00oo]: 접근성 개선 (#71)
    
    * [refactor][jinj00oo]: 공통 응답 shared 레이어로 분리
    
    ---------
    
    Co-authored-by: Chaejy <xhdrp@sookmyung.ac.kr>

---
+++ src/entities/activity-score/api/getActivitySummary.ts
@@ -0,0 +1,15 @@
+import { ActivitySummaryResponse } from '../model/types';
+import { axiosInstance } from '@/shared/lib/axiosInstance';
+
+export const getActivitySummary = async (): Promise<ActivitySummaryResponse['data']> => {
+  try {
+    const response = await axiosInstance.get<ActivitySummaryResponse>(
+      `/v1/user/members/personal-score/pinned5`,
+    );
+    console.log(response.data);
+    return response.data.data;
+  } catch (error) {
+    console.error('Error fetching activity summary:', error);
+    throw error;
+  }
+};
--- src/entities/activity-score/ui/ActivitySummaryItem.tsx
+++ src/entities/activity-score/ui/ActivitySummaryItem.tsx
@@ -16,21 +16,24 @@ export const ActivitySummaryItem: FC<ActivitySummaryItemProps> = ({
   count,
   tooltip,
 }) => {
-  const { activeId, show } = useTooltipStore();
+  const { activeId, show, hide } = useTooltipStore();
   const showTooltip = activeId === id;
 
   return (
     <button
-      type="button"
       className="relative flex cursor-pointer items-center gap-[0.5rem]"
-      aria-describedby={tooltip && showTooltip ? `${id}-tooltip` : undefined}
       onClick={() => show(id, 1500)}
+      onFocus={() => show(id, 1500)}
+      onBlur={() => hide()}
+      onMouseEnter={() => show(id, 1500)}
+      onMouseLeave={() => hide()}
+      aria-describedby={showTooltip ? `tooltip-${id}` : undefined}
     >
       <Icon width="1.5rem" height="1.5rem" />
       <span className="text-body-14-400--2-22 text-foreground-normal">{count}회</span>
       {tooltip && showTooltip && (
         <div
-          id={`${id}-tooltip`}
+          id={`tooltip-${id}`}
           role="tooltip"
           className="absolute bottom-full mb-[0.25rem] w-max"
         >

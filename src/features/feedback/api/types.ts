3d574441c01d0fa11fe48bd3a15615fd6a4fa495
rngrhn4114 <is9206855@gmail.com>
2025-10-04 19:09:37 +0900

    [chore][rngrhn4114]: 공통 응답 형식 shared 레이어로 추출

--- src/features/feedback/api/types.ts
+++ src/features/feedback/api/types.ts
@@ -1,9 +1,5 @@
 // API 공통 응답 형식
-export type CommonResponse<T> = {
-  code: number;
-  message: string;
-  data: T;
-};
+import { CommonResponse } from '@/shared/api/types';
 
 // 피드백 보내기 API 응답 타입
 export type SubmitFeedbackApiResponse = CommonResponse<{

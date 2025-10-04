93a9fde1cbc05d5c5c1af74b86730e39a302d949
rngrhn4114 <is9206855@gmail.com>
2025-10-04 17:39:44 +0900

    [chore][rngrhn4114]: 전처리 로직 주석 설명 추가 및 model 폴더로 추출

--- src/entities/policy/ui/PolicyDetailItem.tsx
+++ src/entities/policy/ui/PolicyDetailItem.tsx
@@ -2,46 +2,7 @@
 
 import { POLICY_ITEMS } from '../model/constants';
 import { Markdown } from '@/entities/policy/ui/Markdown';
-
-function preprocessToMarkdown(src: string, policyId?: string) {
-  let out = src;
-
-  if (policyId === 'PersonalInfoPolicy') {
-    // 1항만 h2 + my-0
-    out = out.replace(
-      /^\s*>?\s*1\.\s*수집하는\s+개인정보\s+항목\s*$/gm,
-      '## 1. 수집하는 개인정보 항목 [my-0]',
-    );
-
-    // 2~9항은 h2 + mb-0
-    out = out.replace(/^\s*>?\s*([2-9])\.\s+(.+)\s*$/gm, '## $1. $2 [mb-0]');
-  }
-
-  if (policyId === 'ServicePolicy') {
-    out = out
-      .replace(/^제1장\s+총칙\s*$/gm, '## 제1장 총칙 [no-mt]')
-      .replace(/^제\s*(\d+)장\s+(.+)\s*$/gm, '## 제$1장 $2')
-      .replace(/^제\s*(\d+)조\s*\((.+)\)\s*$/gm, '### 제$1조 ($2)');
-  }
-
-  if (policyId === 'MarketingPolicy') {
-    // 1항만 h2 + my-0
-    out = out.replace(
-      /^\s*>?\s*1\.\s*수신\s+채널\s+및\s+범위\s*$/gm,
-      '## 1. 수신 채널 및 범위 [my-0]',
-    );
-    // 2~8항은 h2 + mb-0
-    out = out.replace(/^\s*>?\s*([2-8])\.\s+(.+)\s*$/gm, '## $1. $2 [mb-0]');
-
-    // ① 로 시작하는 것만 h2 + my-0
-    out = out.replace(/^\s*>?\s*①\.\s*채널별\s+수신\s+동의\s*$/gm, '## ① 채널별 수신 동의 [my-0]');
-    // ②, ③은 h2 + mb-0
-    out = out.replace(/^\s*>?\s*②\.?\s*(.+)\s*$/gm, '## ② $1 [mb-0]');
-    out = out.replace(/^\s*>?\s*③\.?\s*(.+)\s*$/gm, '## ③ $1 [mb-0]');
-  }
-
-  return out;
-}
+import { preprocessToMarkdown } from '../model/preprocessMarkdown';
 
 type PolicyDetailItemProps = {
   policyId?: string;

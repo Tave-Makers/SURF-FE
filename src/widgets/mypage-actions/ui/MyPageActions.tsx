5c903ae7eb73d5fffbf4dbe8d3d5506d9a3d3912
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-04 17:51:00 +0900

    [fix][chaejy]: 배너 라우팅 연결

--- src/widgets/mypage-actions/ui/MyPageActions.tsx
+++ src/widgets/mypage-actions/ui/MyPageActions.tsx
@@ -17,7 +17,11 @@ export function MyPageActions({ isActive, bannerPart, bannerScore }: Props) {
   return (
     <section className="flex flex-col gap-[1.25rem] px-[1rem] pb-[1.25rem]">
       {isActive && bannerPart && (
-        <Banner part={bannerPart} score={bannerScore} onClickMore={() => {}} />
+        <Banner
+          part={bannerPart}
+          score={bannerScore}
+          onClickMore={() => router.push('/mypage/activity-score')}
+        />
       )}
       <SolidButton size="s" variant="secondary" onClick={() => router.push('/mypage/edit')}>
         프로필 편집

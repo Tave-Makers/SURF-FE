755001a3a8bd3e132f375c96644597c1a5238b92
jinj00oo <icems0428@naver.com>
2025-10-05 23:04:28 +0900

    [refactor][jinj00oo]: sublayout 내 높이 일관성 유지 (#104)

--- src/app-pages/bylaws/ui/BylawsPage.tsx
+++ src/app-pages/bylaws/ui/BylawsPage.tsx
@@ -3,7 +3,7 @@ import { bylawsData } from '@/app-pages/bylaws/model/data';
 
 export default function BylawsPage() {
   return (
-    <div className="flex h-dvh flex-col overflow-y-auto pt-[1.25rem]">
+    <div className="flex h-full flex-col overflow-y-auto pt-[1.25rem]">
       <AccordionGroup accordions={bylawsData} />
     </div>
   );

013b2149460d4c0ae7defc37f4ac5e769c4f3751
jinj00oo <icems0428@naver.com>
2025-10-06 23:25:07 +0900

    [feat][jinj00oo]: personal_score_view 로그 타입 정의 (#109)

---
+++ src/features/activity-score/model/types.ts
@@ -0,0 +1,15 @@
+/**
+ * 이벤트 이름
+ */
+export const ACTIVITY_SCORE_EVENTS = {
+  PERSONAL_SCORE_VIEW: 'personal_score_view',
+} as const;
+
+/**
+ * 이벤트별 속성 타입 매핑
+ */
+export type ActivityScoreEventPropsMap = {
+  [ACTIVITY_SCORE_EVENTS.PERSONAL_SCORE_VIEW]: {
+    total_score: number;
+  };
+};

e64b6bd8326cdf832c29b6ef4748d2f2c8f1897b
jinj00oo <icems0428@naver.com>
2025-10-07 00:04:04 +0900

    [chore][jinj00oo]: 반환값 명시 (#110)

--- src/shared/lib/createDomainTracker.ts
+++ src/shared/lib/createDomainTracker.ts
@@ -3,8 +3,13 @@ import { trackEvent } from './trackEvent';
 /**
  * 도메인별 트래킹 함수를 생성하는 팩토리
  */
-export function createDomainTracker<const M extends Record<string, Record<string, unknown>>>() {
-  return function <K extends keyof M>(eventName: K, eventProps: M[K]) {
-    trackEvent<M, K>(eventName, eventProps);
+export function createDomainTracker<const M extends Record<string, Record<string, unknown>>>(): <
+  K extends keyof M,
+>(
+  eventName: K,
+  eventProps: M[K],
+) => void {
+  return function <K extends keyof M>(eventName: K, eventProps: M[K]): void {
+    return trackEvent<M, K>(eventName, eventProps);
   };
 }

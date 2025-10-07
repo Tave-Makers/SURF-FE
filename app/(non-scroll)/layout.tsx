574ae07ded5c17a9abf83f7c6e819f3482caa2e7
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-07 17:51:03 +0900

    [feat][chaejy]: 스크롤 고정 페이지 레이아웃 제작

---
+++ app/(non-scroll)/layout.tsx
@@ -0,0 +1,12 @@
+import { AppHeader } from '@/widgets/header/ui/AppHeader';
+import { AppNavigation } from '@/widgets/navigation/ui/AppNavigation';
+
+export default function NonScrolllLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-dvh w-dvw flex-col">
+      <AppHeader />
+      <section className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</section>
+      <AppNavigation />
+    </div>
+  );
+}

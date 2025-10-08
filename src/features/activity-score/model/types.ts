1d23c6cdb2135d4e11bac0e56b652d4e491809d9
jinj00oo <icems0428@naver.com>
2025-10-08 22:03:33 +0900

    [feat][jinj00oo]: 액티비티 뷰 이벤트 타입 정의 (#109)

--- src/features/activity-score/model/types.ts
+++ src/features/activity-score/model/types.ts
@@ -2,6 +2,7 @@
  * 이벤트 이름
  */
 export const ACTIVITY_SCORE_EVENTS = {
+  ACTIVITY_VIEW: 'activity_view',
   PERSONAL_SCORE_VIEW: 'personal_score_view',
 } as const;
 
@@ -9,6 +10,9 @@ export const ACTIVITY_SCORE_EVENTS = {
  * 이벤트별 속성 타입 매핑
  */
 export type ActivityScoreEventPropsMap = {
+  [ACTIVITY_SCORE_EVENTS.ACTIVITY_VIEW]: {
+    member_id: number;
+  };
   [ACTIVITY_SCORE_EVENTS.PERSONAL_SCORE_VIEW]: {
     total_score: number;
   };

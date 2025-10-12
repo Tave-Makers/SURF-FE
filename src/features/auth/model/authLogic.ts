dd3e68da5a583250cd3e04b01a5ead14091af05f
DaheeJang <jecydahee77@gmail.com>
2025-10-12 13:13:39 +0900

    [feat][jecy77]: 로그인 앰플리튜드 로그 (#122)

--- src/features/auth/model/authLogic.ts
+++ src/features/auth/model/authLogic.ts
@@ -2,20 +2,46 @@ import { getValidStatus } from '../api/getValidStatus';
 import { getKakaoLoginCallback } from '../api/getKakaoLoginCallback';
 
 import { useAuthStore } from './useAuthStore';
+import { trackAuthEvent } from '../lib/trackAuthEvent';
+import { AUTH_EVENTS } from './types';
+import axios from 'axios';
+import { ErrorResponse } from '@/shared/lib/handleApiError';
 
 export async function handleKakaoLoginCallback(code: string): Promise<string> {
   const setAuth = useAuthStore.getState().setAuth;
 
-  // 1. 카카오 로그인 콜백
-  const res = await getKakaoLoginCallback(code);
-  setAuth({
-    accessToken: res.data.accessToken,
-    nickname: res.data.nickname,
-    email: res.data.email,
-    profileImageUrl: res.data.profileImageUrl,
-  });
+  // 콜백 이벤트 트래킹
+  trackAuthEvent(AUTH_EVENTS.LOGIN_CALLBACK, { code_length: code.length });
 
-  // 2. 온보딩 여부 확인
-  const validStatus = await getValidStatus();
-  return validStatus.data ? '/onboarding' : '/home';
+  try {
+    // 1. 카카오 로그인 콜백
+    const res = await getKakaoLoginCallback(code);
+
+    // 로그인 성공 이벤트 트래킹
+    trackAuthEvent(AUTH_EVENTS.LOGIN_SUCCESS, {
+      user_id: res.data.email ?? 'unknown',
+    });
+
+    setAuth({
+      accessToken: res.data.accessToken,
+      nickname: res.data.nickname,
+      email: res.data.email,
+      profileImageUrl: res.data.profileImageUrl,
+    });
+
+    // 2. 온보딩 여부 확인
+    const validStatus = await getValidStatus();
+    return validStatus.data ? '/onboarding' : '/home';
+  } catch (error) {
+    if (axios.isAxiosError<ErrorResponse>(error)) {
+      // axios 에러인 경우
+      const statusCode = error.response?.status ?? 500;
+      trackAuthEvent(AUTH_EVENTS.LOGIN_FAIL, { error_code: statusCode });
+    } else {
+      // axios 외의 에러
+      trackAuthEvent(AUTH_EVENTS.LOGIN_FAIL, { error_code: 500 });
+    }
+
+    throw error;
+  }
 }
--- src/features/auth/model/types.ts
+++ src/features/auth/model/types.ts
@@ -11,19 +11,21 @@ export type AuthState = AuthData & {
 };
 
 /**
- *  이벤트 이름
+ *  Amplitude 로그인 이벤트 이름
  */
 export const AUTH_EVENTS = {
   LOGIN_KAKAO_CLICK: 'login_kakao',
-  // LOGIN_SUCCESS: 'login_success',
-  // LOGIN_FAIL: 'login_fail',
+  LOGIN_CALLBACK: 'login_callback',
+  LOGIN_SUCCESS: 'login_success',
+  LOGIN_FAIL: 'login_fail',
 } as const;
 
 /**
- * 이벤트별 속성 타입 매핑
+ * Amplitude 이벤트별 속성 타입 매핑
  */
 export type AuthEventPropsMap = {
   [AUTH_EVENTS.LOGIN_KAKAO_CLICK]: { login_method: string };
-  // [AUTH_EVENTS.LOGIN_SUCCESS]: { member_id: string };
-  // [AUTH_EVENTS.LOGIN_FAIL]: { error_code: number };
+  [AUTH_EVENTS.LOGIN_CALLBACK]: { code_length: number };
+  [AUTH_EVENTS.LOGIN_SUCCESS]: { user_id: string };
+  [AUTH_EVENTS.LOGIN_FAIL]: { error_code: number };
 };

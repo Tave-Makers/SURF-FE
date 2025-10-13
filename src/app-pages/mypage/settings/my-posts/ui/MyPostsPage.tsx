05862608a18094c3258dfdd03567e012a4514ab6
Seoyeon <130418820+rngrhn4114@users.noreply.github.com>
2025-10-13 22:38:45 +0900

    [HOTFIX] 게시글 목록 조회 화면 스크롤 버그 수정 (#130)
    * [fix][rngrhn4114]: 게시글 목록 조회 화면 스크롤 버그 수정
    
    * [chore][rngrhn4114]: 스크롤바 잘리는 문제 수정
    
    * [chore][rngrhn4114]: 이용약관 각 페이지당 pb 3.5rem으로 통일

--- src/app-pages/mypage/settings/my-posts/ui/MyPostsPage.tsx
+++ src/app-pages/mypage/settings/my-posts/ui/MyPostsPage.tsx
@@ -4,5 +4,9 @@ import { useInfiniteMyPosts } from '@/features/post/model/useMyPosts';
 import { PostListPage } from '@/widgets/post-list/ui/PostListPage';
 
 export default function MyPostsPage() {
-  return <PostListPage useInfiniteQueryHook={useInfiniteMyPosts} />;
+  return (
+    <div className="flex h-full">
+      <PostListPage useInfiniteQueryHook={useInfiniteMyPosts} />
+    </div>
+  );
 }

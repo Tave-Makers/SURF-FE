ce69da629cd1e00e6a0d32e504c574d6e8282645
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-19 10:42:04 +0900

    [fix][chaejy]: main 레이아웃 수정

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,11 @@
 import { AppHeader } from '@/widgets/header/ui/AppHeader';
 import { AppNavigation } from '@/widgets/navigation/ui/AppNavigation';
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+export default function NonScrollLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex h-dvh w-full flex-col">
       <AppHeader />
-      <section className="flex-1">{children}</section>
+      <section className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</section>
       <AppNavigation />
     </div>
   );

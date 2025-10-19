2c97c08186958117750af5699acdeba85a3fe5a4
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-19 11:02:38 +0900

    [chore][chaejy]: 페이지 주석 처리

---
+++ app/(main)/mypage/edit/page.tsx
@@ -0,0 +1,4 @@
+// import { MyEditPage } from '@/app-pages/mypage/edit/ui/MyEditPage';
+// export default function Page() {
+//   return <MyEditPage />;
+// }
---
+++ src/app-pages/mypage/edit/ui/MyEditPage.tsx
@@ -0,0 +1,33 @@
+// 'use client';
+
+// //import { useState } from 'react';
+// //import { ProfileEditHeader } from '@/widgets/profile-edit-header/ui/ProfileEditHeader';
+// //import { ProfileEditForm } from '@/widgets/profile-edit-form/ui/ProfileEditForm';
+// import { TextButton } from '@/shared/ui/text-button/TextButton';
+
+// export function MyEditPage() {
+//   //const [memberName] = useState('김테이비');
+
+//   const handleEdit = () => {
+//     // TODO: 편집 저장/전환 로직 또는 라우팅 연결
+//   };
+
+//   const handleAddCareer = () => {
+//     // TODO: 경력 필드 추가 로직
+//   };
+
+//   return (
+//     <div className="flex flex-col">
+//       <div className="flex w-full flex-col gap-[0.5rem] px-[1rem] py-[0.62rem]">
+//         <div className="flex w-[4rem] self-end">
+//           <TextButton size="s" variant="secondary" onClick={handleEdit}>
+//             삭제하기
+//           </TextButton>
+//         </div>
+//         <TextButton size="m" variant="secondary" onClick={handleAddCareer}>
+//           경력 추가하기
+//         </TextButton>
+//       </div>
+//     </div>
+//   );
+// }

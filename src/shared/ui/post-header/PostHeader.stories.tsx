2915de99dd9553744df64cc9191e3798d9188aa9
DaheeJang <jecydahee77@gmail.com>
2025-10-26 15:28:34 +0900

    [feat][jecy77]: PostHeader 컴포넌트 제작 완료 (#151)

--- src/shared/ui/post-header/PostHeader.stories.tsx
+++ src/shared/ui/post-header/PostHeader.stories.tsx
@@ -5,23 +5,29 @@ const meta: Meta<typeof PostHeader> = {
   title: 'Shared/UI/PostHeader',
   component: PostHeader,
   tags: ['autodocs'],
-  parameters: {
-    layout: 'centered',
-    controls: { include: ['size'] },
-  },
   argTypes: {
     title: { control: 'text' },
     category: { control: 'text' },
     subCategory: { control: 'text' },
   },
   args: {
-    title: '포스트 제목',
-    category: '카테고리',
-    subCategory: '서브카테고리',
+    title: '제목',
+    category: '공지사항',
+    subCategory: '행사',
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+/* 검색창 */
+export const Default: Story = {
+  render: (args) => {
+    return <PostHeader {...args} />;
+  },
+  args: {
+    title: '제목',
+    category: '공지사항',
+    subCategory: '행사',
+  },
+};
--- src/shared/ui/post-header/PostHeader.tsx
+++ src/shared/ui/post-header/PostHeader.tsx
@@ -1,16 +1,33 @@
 type PostHeaderProps = {
   title: string;
   category?: string;
-  subCategory?: string;
+  subCategory?: SubCategoryType;
 };
 
+export type SubCategoryType = '전체' | '행사' | '활동' | '제휴' | '릴리즈' | '기타';
+
 export function PostHeader({ title, category, subCategory }: PostHeaderProps) {
+  const categoryClass = 'cursor-pointer underline [text-underline-position:from-font]';
   return (
     <>
-      <div>
-        {category} {subCategory && `> ${subCategory}`}
+      <div className="flex flex-col">
+        <div className="text-caption-caption4 item text-foreground-foreground-normal flex gap-3">
+          {category && (
+            <>
+              {/* 링크는 추후 연결 */}
+              <div className={categoryClass}>{category}</div>
+              {subCategory && (
+                <>
+                  <div>{`>`}</div>
+                  {/* 링크는 추후 연결 */}
+                  <div className={categoryClass}>{subCategory}</div>
+                </>
+              )}
+            </>
+          )}
+        </div>
+        <div className="text-foreground-foreground-normal text-body-body3 pt-10">{title}</div>
       </div>
-      <div>{title}</div>
     </>
   );
 }

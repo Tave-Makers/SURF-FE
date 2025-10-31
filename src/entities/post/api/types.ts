4c5c8035d97cbde7b3a029404f5e81ab83a7a5a9
jinj00oo <icems0428@naver.com>
2025-10-31 17:04:34 +0900

    [fix][jinj00oo]: api 응답 타입 오타 수정

--- src/entities/post/api/types.ts
+++ src/entities/post/api/types.ts
@@ -28,7 +28,7 @@ export type PostContent = {
   boardId: number;
   scrappedByMe: boolean;
   scrapCount: number;
-  likeByMe: boolean;
+  likedByMe: boolean;
   likeCount: number;
   commentCount: number;
   nickname: string;

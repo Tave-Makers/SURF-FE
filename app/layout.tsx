6932a824e3b26d577d44c7c42a65d58acf223418
Jiyeon <144954836+Chaejy@users.noreply.github.com>
2025-11-02 13:34:08 +0900

    [fix][chaejy]: layout conflict 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <AnalyticsProvider />
         <QueryProvider>
           <AuthProvider>
-            <main className="bg-background-background-tertiary box-content flex h-full w-dvw sm:w-[360px]">
-              {children}
-            </main>
+            <PageTrackingProvider>
+              <main className="bg-background-normal box-content flex h-full w-dvw sm:w-[360px]">
+                {children}
+              </main>
+            </PageTrackingProvider>
             <ReactQueryDevtools initialIsOpen={false} />
           </AuthProvider>
         </QueryProvider>

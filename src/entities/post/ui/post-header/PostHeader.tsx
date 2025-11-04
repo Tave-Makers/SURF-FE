e9b13a03c894cf7b21ce4b7a1a374cbc657bca7b
DaheeJang <jecydahee77@gmail.com>
2025-11-04 14:39:52 +0900

    [refactor][jecy77]: 시맨틱 태그 적용 (#151)

--- src/entities/post/ui/post-header/PostHeader.tsx
+++ src/entities/post/ui/post-header/PostHeader.tsx
@@ -9,25 +9,25 @@ export type SubCategoryType = '전체' | '행사' | '활동' | '제휴' | '릴�
 export function PostHeader({ title, category, subCategory }: PostHeaderProps) {
   const categoryClass = 'cursor-pointer underline [text-underline-position:from-font]';
   return (
-    <>
-      <div className="flex flex-col">
-        <div className="text-caption-caption4 text-foreground-foreground-normal flex gap-3">
-          {category && (
+    <header className="flex flex-col">
+      {category && (
+        <nav
+          aria-label="게시글 카테고리 경로"
+          className="text-caption-caption4 text-foreground-foreground-normal flex gap-3"
+        >
+          {/* 링크는 추후 연결 */}
+          <span className={categoryClass}>{category}</span>
+          {subCategory && (
             <>
+              <span aria-hidden="true">{'>'}</span>
               {/* 링크는 추후 연결 */}
-              <div className={categoryClass}>{category}</div>
-              {subCategory && (
-                <>
-                  <div>{`>`}</div>
-                  {/* 링크는 추후 연결 */}
-                  <div className={categoryClass}>{subCategory}</div>
-                </>
-              )}
+              <span className={categoryClass}>{subCategory}</span>
             </>
           )}
-        </div>
-        <div className="text-foreground-foreground-normal text-body-body3 pt-10">{title}</div>
-      </div>
-    </>
+        </nav>
+      )}
+
+      <h1 className="text-foreground-foreground-normal text-body-body3 pt-10">{title}</h1>
+    </header>
   );
 }

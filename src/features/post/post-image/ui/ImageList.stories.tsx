169b73b107977d1dcbd518767a19f39755915e4d
jinj00oo <icems0428@naver.com>
2025-11-08 16:07:51 +0900

    [feat][jinj00oo]: 이미지 포스트 스토리북 제작 (#177)

---
+++ src/features/post/post-image/ui/ImageList.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { ImageList } from './ImageList';
+
+const meta: Meta<typeof ImageList> = {
+  title: 'Features/UI/PostImage/ImageList',
+  component: ImageList,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof ImageList>;
+
+/**
+ * 실제 업로드 / 삭제 / 드래그 모두 가능한 스토리
+ */
+export const Default: Story = {
+  render: () => <ImageList />,
+};

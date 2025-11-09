ef9bac4daf86bbbe2a269f026601d2e641bbc1b3
jinj00oo <icems0428@naver.com>
2025-11-10 00:52:25 +0900

    [chore][jinj00oo]: svgr 연동 (#178)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import type { Configuration, RuleSetRule } from 'webpack';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -8,5 +9,34 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../src/shared/assets'],
+
+  webpackFinal: (config: Configuration) => {
+    if (!config.module || !config.module.rules) {
+      return config;
+    }
+
+    const imageRule = config.module.rules.find((rule) => {
+      if (typeof rule !== 'object' || rule === null || !rule.test) {
+        return false;
+      }
+      if (rule.test instanceof RegExp) {
+        return rule.test.test('.svg');
+      }
+      return false;
+    }) as RuleSetRule | undefined;
+
+    if (imageRule) {
+      imageRule.exclude = /\.svg$/;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.mode = 'development';
+
+    return config;
+  },
 };
 export default config;

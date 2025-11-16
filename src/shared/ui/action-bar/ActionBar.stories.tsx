c0003751584f295f60f539389ee5f72189743152
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-11-16 13:38:38 +0900

    [fix][chaejy]: 스토리북import 경로 수정

--- src/shared/ui/action-bar/ActionBar.stories.tsx
+++ src/shared/ui/action-bar/ActionBar.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import { useRef, useState } from 'react';
 import { ActionBar } from './ActionBar';
 

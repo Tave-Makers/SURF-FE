8dcc85789c7fc03f8ee98f911f9427862ba2bb6f
jinj00oo <icems0428@naver.com>
2025-11-18 00:18:15 +0900

    [chore][jinj00oo]: 게시글 수정 요청 api 함수 및 리액트 쿼리훅 작성 (#201)

---
+++ src/features/post/update-post/api/updatePost.ts
@@ -0,0 +1,11 @@
+import { PostDetailData, PostDetailResponse } from '@/entities/post/api/types';
+import { axiosInstance } from '@/shared/lib/axiosInstance';
+import { UpdatePostRequest } from './type';
+
+export const updatePost = async (
+  postId: number,
+  data: UpdatePostRequest,
+): Promise<PostDetailData> => {
+  const res = await axiosInstance.patch<PostDetailResponse>(`v1/user/posts/${postId}`, data);
+  return res.data.data;
+};

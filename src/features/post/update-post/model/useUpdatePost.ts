323aef14d1fcc8f78f93c6038c321141128d4d6a
jinj00oo <icems0428@naver.com>
2025-11-18 15:48:22 +0900

    [chore][jinj00oo]: 게시글 수정 응답으로 캐싱 덮어쓰기 (#201)

--- src/features/post/update-post/model/useUpdatePost.ts
+++ src/features/post/update-post/model/useUpdatePost.ts
@@ -1,16 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { UpdatePostRequest } from '../api/type';
 import { updatePost } from '../api/updatePost';
+import { transformDetailToPost } from '@/entities/post/model/mappers';
 
 export const useUpdatePost = (postId: number) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (data: UpdatePostRequest) => updatePost(postId, data),
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({
-        queryKey: ['postDetail', postId],
-      });
+    onSuccess: (updatedRes) => {
+      const mappedRes = transformDetailToPost(updatedRes);
+      queryClient.setQueryData(['postDetail', postId], mappedRes);
     },
   });
 };

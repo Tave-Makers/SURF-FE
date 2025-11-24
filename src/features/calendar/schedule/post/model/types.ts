c9c95a83213100cb0c425e887d4028985bd3582a
rngrhn4114 <is9206855@gmail.com>
2025-11-25 00:35:47 +0900

    [feat][rngrhn4114]: content 타입 제거

--- src/features/calendar/schedule/post/model/types.ts
+++ src/features/calendar/schedule/post/model/types.ts
@@ -3,7 +3,6 @@ import { ScheduleCategory } from '@/entities/schedule/model/types';
 export type ScheduleFormData = {
   category: ScheduleCategory;
   title: string;
-  content?: string;
   startDate: Date;
   endDate: Date;
   location?: string;

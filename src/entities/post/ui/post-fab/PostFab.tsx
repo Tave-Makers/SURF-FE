6e19282951c9231dde1502b04f5b2f59a0b86d26
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-11-25 23:55:43 +0900

    [feat][chaejy]: base style 상수화

--- src/entities/post/ui/post-fab/PostFab.tsx
+++ src/entities/post/ui/post-fab/PostFab.tsx
@@ -5,14 +5,12 @@ type PostFabProps = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
+const postFabClass =
+  'bg-background-background-primary hover:bg-background-background-primary-darker active:bg-background-background-primary-darker rounded-max flex p-13 transition-colors duration-200 focus-visible:ring-2 focus-visible:outline-none';
+
 export const PostFab = ({ onClick }: PostFabProps) => {
   return (
-    <button
-      type="button"
-      className="bg-background-background-primary hover:bg-background-background-primary-darker active:bg-background-background-primary-darker rounded-max flex p-13 transition-colors duration-200 focus-visible:ring-2 focus-visible:outline-none"
-      onClick={onClick}
-      aria-label="게시글 작성 버튼"
-    >
+    <button type="button" className={postFabClass} onClick={onClick} aria-label="게시글 작성 버튼">
       <SurfIcon name="Edit" className="text-foreground-foreground-accent" />
     </button>
   );

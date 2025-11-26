490e66a0eab333bea66059bdea6fb9b6163c5485
rngrhn4114 <is9206855@gmail.com>
2025-11-26 23:58:46 +0900

    [feat][rngrhn4114]: 일정 생성 Fab 매니저 이상 레벨만 보이도록 수정

--- src/app-pages/home/calendar/ui/CalendarPage.tsx
+++ src/app-pages/home/calendar/ui/CalendarPage.tsx
@@ -5,10 +5,12 @@ import { useRouter } from 'next/navigation';
 import Calendar from '@/widgets/calendar/ui/Calendar';
 import { useCalendarSchedule } from '@/features/calendar/model/useCalendarSchedule';
 import { PostFab } from '@/entities/post/ui/post-fab/PostFab';
+import { useAuthStore } from '@/features/auth/model/useAuthStore';
 
 export function CalendarPage() {
   const router = useRouter();
   const [month, setMonth] = useState<Date>(new Date());
+  const memberRole = useAuthStore((s) => s.memberRole);
   const { data: schedules = {} } = useCalendarSchedule(month.getFullYear(), month.getMonth() + 1);
 
   const handleCreateSchedule = () => {
@@ -21,9 +23,13 @@ export function CalendarPage() {
         <Calendar month={month} onMonthChange={setMonth} schedules={schedules} />
       </div>
 
-      <div className="absolute right-15 bottom-15 z-50">
-        <PostFab onClick={handleCreateSchedule} />
-      </div>
+      {(memberRole === 'superManager' ||
+        memberRole === 'executiveManager' ||
+        memberRole === 'manager') && (
+        <div className="absolute right-15 bottom-15 z-50">
+          <PostFab onClick={handleCreateSchedule} />
+        </div>
+      )}
     </div>
   );
 }

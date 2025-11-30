ac9d5ffc83a4bbb58a30028f76344b7c10028864
jinj00oo <icems0428@naver.com>
2025-11-30 14:25:57 +0900

    [chore][jinj00oo]: 글자수 제한 적용

--- src/features/post/post-editor/config/tiptap.ts
+++ src/features/post/post-editor/config/tiptap.ts
@@ -1,11 +1,17 @@
 import StarterKit from '@tiptap/starter-kit';
 import { TextStyleKit } from '@tiptap/extension-text-style';
 import Placeholder from '@tiptap/extension-placeholder';
+import CharacterCount from '@tiptap/extension-character-count';
+
+const MAX_LENTH = 100000;
 
 export const POST_EDITOR_EXTENSIONS = [
   StarterKit,
   TextStyleKit,
   Placeholder.configure({
     placeholder: '글, 제목, 내용을 입력해주세요.',
+  }),
+  CharacterCount.configure({
+    limit: MAX_LENTH,
   }),
 ];

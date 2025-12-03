20c190d61a79b5e7ce1da94c568648242c091bf3
Dahee Jang <jecy123@naver.com>
2025-12-03 14:27:35 +0900

    chore: try-catch문 제거

--- src/features/post/api/getPostDetail.ts
+++ src/features/post/api/getPostDetail.ts
@@ -7,11 +7,6 @@ import { PostDetailResponse } from '@/entities/post/api/types';
  * @returns 게시글 상세 정보(PostDetailResponse['data'])
  */
 export const getPostDetail = async (postId: number): Promise<PostDetailResponse['data']> => {
-  try {
-    const response = await axiosInstance.get<PostDetailResponse>(`/v1/user/posts/${postId}`);
-    return response.data.data;
-  } catch (error) {
-    console.error('Error fetching post detail:', error);
-    throw error;
-  }
+  const response = await axiosInstance.get<PostDetailResponse>(`/v1/user/posts/${postId}`);
+  return response.data.data;
 };

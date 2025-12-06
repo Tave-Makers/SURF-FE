3faaad4eb49f69b41abd0a61136a728e31df22db
rngrhn4114 <is9206855@gmail.com>
2025-12-06 19:31:58 +0900

    feat: 게시글의 일정 정보 get api 요청 작성

---
+++ src/features/post/model/useGetPostScheduleQuery.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import { PostScheduleResponse } from '@/entities/post/api/types';
+import { getPostSchedule } from '../api/getPostSchedule';
+
+export function useGetPostScheduleQuery(postId: number, enabled = true) {
+  return useQuery<PostScheduleResponse['data']>({
+    queryKey: ['postSchedule', postId],
+    queryFn: () => getPostSchedule(postId),
+    enabled,
+  });
+}

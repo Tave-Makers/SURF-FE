8b2d10667ec236e6997e3c6f3f4d3db46caf3e42
rngrhn4114 <is9206855@gmail.com>
2025-12-12 03:18:45 +0900

    feat: MenuItem 컴포넌트 제작

---
+++ src/shared/ui/menu-item/MenuItem.tsx
@@ -0,0 +1,27 @@
+/**
+ * MenuItem 컴포넌트
+ * @param label - MenuItem 텍스트
+ * @param onClick - MenuItem 클릭 콜백함수
+ * @param isSelected - MenuItem 선택 여부
+ */
+
+type MenuItemProps = {
+  label: string;
+  onClick: () => void;
+  isSelected?: boolean;
+};
+
+export function MenuItem({ label, onClick, isSelected = false }: MenuItemProps) {
+  const menuItemBaseStyle =
+    'rounded-3 flex h-[1.87rem] min-w-[4.5rem] items-center justify-center px-10 bg-background-normal-lighter active:bg-background-quaternary';
+
+  const labelBaseStyle = isSelected
+    ? 'text-foreground-primary text-body11'
+    : 'text-foreground-normal-lighter text-body11';
+
+  return (
+    <button type="button" onClick={onClick} className={menuItemBaseStyle}>
+      <span className={labelBaseStyle}>{label}</span>
+    </button>
+  );
+}

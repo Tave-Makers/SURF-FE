4debf0d42a57f26a08c7341ac1d29bddbf44aef7
rngrhn4114 <is9206855@gmail.com>
2025-12-13 01:14:52 +0900

    feat: MenuDropDown 컴포넌트 제작
    - items: MenuDropDown 내의 아이템 리스트
    - renderItem: MenuDropDown 내의 각 아이템을 어떻게 렌더링할지 주입
    - onItemClick: MenuDropDown 내의 각 아이템 클릭 이벤트

---
+++ src/shared/ui/menu-dropdown/MenuDropDown.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import { MenuDropDown } from './MenuDropDown';
+
+type StoryItem = {
+  id: number;
+  label: string;
+  type: 'default' | 'active' | 'selected';
+};
+
+const meta: Meta<typeof MenuDropDown<StoryItem>> = {
+  title: 'Shared/UI/MenuDropDown',
+  component: MenuDropDown,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof MenuDropDown<StoryItem>>;
+
+// 더미 데이터
+const MOCK_ITEMS: StoryItem[] = [
+  { id: 1, label: 'default', type: 'default' },
+  { id: 2, label: 'active clicked', type: 'active' },
+  { id: 3, label: 'selected', type: 'selected' },
+  { id: 4, label: 'default', type: 'default' },
+];
+
+export const Default: Story = {
+  args: {
+    items: MOCK_ITEMS,
+    onItemClick: (item) => alert(`Clicked ${item.label}`),
+
+    renderItem: (item, onItemClick) => {
+      let className = 'w-full px-10 text-sm rounded-3 text-body-body11 ';
+
+      if (item.type === 'active') {
+        className += 'bg-background-quaternary text-foreground-secondary';
+      } else if (item.type === 'selected') {
+        className += 'text-foreground-primary';
+      } else {
+        className += 'text-foreground-secondary';
+      }
+
+      return (
+        <div
+          role="button"
+          className={className}
+          onClick={onItemClick}
+          tabIndex={0}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter' || e.key === ' ') {
+              e.preventDefault();
+              onItemClick();
+            }
+          }}
+        >
+          {item.label}
+        </div>
+      );
+    },
+  },
+};
---
+++ src/shared/ui/menu-dropdown/MenuDropDown.tsx
@@ -0,0 +1,29 @@
+import type { ReactNode } from 'react';
+
+/**
+ * @param items - MenuDropDown 내의 아이템 리스트
+ * @param renderItem - MenuDropDown 내의 각 아이템을 어떻게 렌더링할지 주입
+ * @param onItemClick - MenuDropDown 내의 각 아이템 클릭 이벤트
+ */
+
+type MenuDropDownProps<T extends { id: number }> = {
+  items: T[];
+  renderItem: (item: T, onClick: () => void) => ReactNode;
+  onItemClick: (item: T) => void;
+};
+
+export function MenuDropDown<T extends { id: number }>({
+  items,
+  renderItem,
+  onItemClick,
+}: MenuDropDownProps<T>) {
+  return (
+    <div className="rounded-3 bg-background-normal-lighter flex flex-col items-center p-5">
+      {items.map((item) => (
+        <div key={item.id} className="w-full">
+          {renderItem(item, () => onItemClick(item))}
+        </div>
+      ))}
+    </div>
+  );
+}

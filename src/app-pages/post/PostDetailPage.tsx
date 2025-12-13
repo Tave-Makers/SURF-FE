64707c38c243908bb9710690f1bcc14b1e82fc83
jinj00oo <icems0428@naver.com>
2025-12-13 14:20:12 +0900

    chore: 알럿 삭제

--- src/app-pages/post/PostDetailPage.tsx
+++ src/app-pages/post/PostDetailPage.tsx
@@ -72,12 +72,9 @@ export default function PostDetailPage({ postId }: PostDetailPageProps) {
   const handleDelete = async () => {
     try {
       await deletePost(numericPostId);
-
-      alert('게시글이 삭제되었습니다.');
       setShowDeleteAlert(false);
       router.back();
     } catch (e) {
-      alert('삭제에 실패했습니다. 다시 시도해주세요.');
       console.error(e);
     }
   };

ab602814c12edeeca866e88e94f077b868cee9ad
Jiyeon <144954836+Chaejy@users.noreply.github.com>
2025-12-21 17:17:31 +0900

    feat: max-width 추가

--- src/shared/ui/toast/ToastViewport.tsx
+++ src/shared/ui/toast/ToastViewport.tsx
@@ -5,7 +5,7 @@ import Toast from './Toast';
 import { ToastStore, useToastStore } from '@/shared/store/toastStore';
 
 const viewportStyle =
-  'fixed bottom-0 left-1/2 z-50 flex w-full -translate-x-1/2 flex-col px-10 pb-[5.75rem]';
+  'sm:max-w-[360px] fixed bottom-0 left-1/2 z-50 flex w-full -translate-x-1/2 flex-col px-10 pb-[5.75rem]';
 
 const ToastViewport = () => {
   const current = useToastStore((s: ToastStore) => s.current);

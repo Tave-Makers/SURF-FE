b10cee589fcd77d5b8cc43f186ad19f09af1d421
Dahee Jang <jecy123@naver.com>
2025-12-21 15:28:51 +0900

    style: 디자인 토큰명 수정

--- src/shared/ui/shortcut/Shortcut.tsx
+++ src/shared/ui/shortcut/Shortcut.tsx
@@ -10,7 +10,7 @@ export const Shortcut = ({ type, label, imageSrc }: ShortcutProps) => {
   if (type === 'circle') {
     return (
       <div className="flex flex-col items-center gap-7">
-        <div className="h-[40px] w-[40px] overflow-hidden rounded-full bg-gray-200">
+        <div className="h-[2.5rem] w-[2.5rem] overflow-hidden rounded-full bg-gray-200">
           {imageSrc && (
             <Image src={imageSrc} alt={label} width={40} height={40} className="object-cover" />
           )}
@@ -22,9 +22,9 @@ export const Shortcut = ({ type, label, imageSrc }: ShortcutProps) => {
 
   // rectangle
   return (
-    <div className="bg-background-background-normal-lighter rounded-5 border-border-border-secondary flex h-[150px] w-[105px] flex-col overflow-hidden border">
+    <div className="bg-background-normal-lighter rounded-5 border-border-secondary flex h-[9.375rem] w-[6.5625rem] flex-col overflow-hidden border">
       {/* Label 영역 */}
-      <div className="text-foreground-foreground-normal text-body-body5 px-13 pt-13">{label}</div>
+      <div className="text-foreground-normal text-body-body5 px-13 pt-13">{label}</div>
 
       {/* 이미지 영역 */}
       <div className="flex-1 bg-gray-200">

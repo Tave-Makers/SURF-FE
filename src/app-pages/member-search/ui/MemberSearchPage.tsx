6ed76753226ee6b5596a73fde8030608129856dc
jinj00oo <icems0428@naver.com>
2025-12-26 23:09:13 +0900

    feat: MemberSearchPage 제작

--- src/app-pages/member-search/ui/MemberSearchPage.tsx
+++ src/app-pages/member-search/ui/MemberSearchPage.tsx
@@ -1,9 +1,20 @@
+'use client';
+
+import { useMemberSearch } from '@/features/member-search/api/useMemberSearch';
+import { useMemberFilters } from '@/features/member-search/model/useMemberFilters';
 import { HeaderMode } from '@/shared/ui/header/Header';
 import { AppHeader } from '@/widgets/header/ui/AppHeader';
+import { MemberListWidget } from '@/widgets/member-search/ui/MemberListWidget';
+import { MemberSearchWidget } from '@/widgets/member-search/ui/MemberSearchWidget';
 
 export function MemberSearchPage() {
+  const filters = useMemberFilters();
+  const { keyword, generation, part } = filters;
+
+  const queryResult = useMemberSearch({ keyword, generation, part });
+
   return (
-    <div>
+    <div className="flex h-full flex-col">
       <AppHeader
         overrideHeader={{
           mode: HeaderMode.Default,
@@ -11,6 +22,10 @@ export function MemberSearchPage() {
           hasLeftIcon: true,
         }}
       />
+      <MemberSearchWidget filters={filters} totalCount={queryResult.totalCount} />
+      <main className="flex-1 overflow-y-auto">
+        <MemberListWidget queryResult={queryResult} />
+      </main>
     </div>
   );
 }

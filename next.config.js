d1f78a8ddc1cb9b464256d0c8cba555d917b6304
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-12-28 15:17:40 +0900

    fix: s3 주소 허용

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    reactCompiler: true
+    reactCompiler: true,
   },
 
   turbopack: {
-    // Turbopack 전용 설정
     rules: {
       '*.svg': {
         loaders: ['@svgr/webpack'],
@@ -14,68 +13,69 @@ const nextConfig = {
     },
   },
 
-images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'k.kakaocdn.net',
-      pathname: '/**',
-    },
-    {
-      protocol: 'http',
-      hostname: 'k.kakaocdn.net',
-      pathname: '/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'img1.kakaocdn.net',
-      pathname: '/**',
-    },
-    {
-      protocol: 'http',
-      hostname: 'img1.kakaocdn.net',
-      pathname: '/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 't1.kakaocdn.net',
-      pathname: '/**',
-    },
-    {
-      protocol: 'http',
-      hostname: 't1.kakaocdn.net',
-      pathname: '/**',
-    },
-  ],
-},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'k.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img1.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'img1.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 't1.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 't1.kakaocdn.net',
+        pathname: '/**',
+      },
 
+      {
+        protocol: 'https',
+        hostname: 'tavesurf-dev01.s3.ap-northeast-2.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
 
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test?.test?.('.svg')
+      (rule) => rule.test?.test?.('.svg'),
     );
-    
-    // fileLoaderRule이 없으면 안전 가드 처리
+
     if (!fileLoaderRule) {
-      // 1) *.svg?url → 그냥 자산으로 처리
       config.module.rules.push({
         test: /\.svg$/i,
         resourceQuery: /url/,
         type: 'asset/resource',
       });
-    
-      // 2) 나머지 → SVGR 컴포넌트로 처리
+
       config.module.rules.push({
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         resourceQuery: { not: [/url/] },
         use: ['@svgr/webpack'],
       });
-    
+
       return config;
     }
-    
-    // 정상적으로 fileLoaderRule을 찾은 경우 원래 방식 수행
+
     fileLoaderRule.exclude = /\.svg$/i;
     return config;
   },

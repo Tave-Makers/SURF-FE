8244c04b64b038914d4f5bd57b0df12f988f41ce
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-12-28 15:48:04 +0900

    fix: s3 경로 상수화

--- next.config.js
+++ next.config.js
@@ -1,63 +1,50 @@
 /** @type {import('next').NextConfig} */
+
+const KAKAO_CDN_HOSTS = ['k.kakaocdn.net', 'img1.kakaocdn.net', 't1.kakaocdn.net'];
+
+function hostnameFromUrl(url) {
+  if (!url) return null;
+  try {
+    return new URL(url).hostname;
+  } catch {
+    if (/^[a-z0-9.-]+$/i.test(url)) return url;
+    return null;
+  }
+}
+
+const S3_HOSTNAME = hostnameFromUrl(process.env.NEXT_PUBLIC_S3_BUCKET_URL);
+
 const nextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
+  experimental: { reactCompiler: true },
 
   turbopack: {
     rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
-      },
+      '*.svg': { loaders: ['@svgr/webpack'], as: '*.js' },
     },
   },
 
   images: {
     remotePatterns: [
-      {
+      ...KAKAO_CDN_HOSTS.map((hostname) => ({
         protocol: 'https',
-        hostname: 'k.kakaocdn.net',
+        hostname,
         pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'k.kakaocdn.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img1.kakaocdn.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'img1.kakaocdn.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 't1.kakaocdn.net',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 't1.kakaocdn.net',
-        pathname: '/**',
-      },
+      })),
 
-      {
-        protocol: 'https',
-        hostname: 'tavesurf-dev01.s3.ap-northeast-2.amazonaws.com',
-        pathname: '/**',
-      },
+      ...(S3_HOSTNAME
+        ? [
+            {
+              protocol: 'https',
+              hostname: S3_HOSTNAME,
+              pathname: '/**',
+            },
+          ]
+        : []),
     ],
   },
 
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test?.test?.('.svg'),
-    );
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
     if (!fileLoaderRule) {
       config.module.rules.push({

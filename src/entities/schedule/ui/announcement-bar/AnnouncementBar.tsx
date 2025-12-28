e22f9c6c8edf3625d0290f83170367d40f5edd34
Dahee Jang <jecy123@naver.com>
2025-12-28 14:24:50 +0900

    fix: AnnoucementBar 컴포넌트 date 타입 string으로 변경

--- src/entities/schedule/ui/announcement-bar/AnnouncementBar.tsx
+++ src/entities/schedule/ui/announcement-bar/AnnouncementBar.tsx
@@ -1,10 +1,9 @@
 import { ActivityCategory } from '@/entities/calendar/model/types';
-import { formatMonthDay } from '@/shared/utils/date';
 import { CalendarBadge } from '@/entities/calendar/ui/CalendarBadge/CalendarBadge';
 
 interface AnnouncementBarProps {
   title: string;
-  date: Date;
+  date: string;
   category: ActivityCategory;
   onClick?: () => void;
 }
@@ -15,7 +14,7 @@ export const AnnouncementBar = ({ title, date, category, onClick }: Announcement
       className="text-body-body9 text-foreground-normal bg-background-normal-lighter rounded-5 border-border-secondary flex h-[3.125rem] w-full items-center gap-10 border px-13 shadow-[0_0_20px_3px_rgba(0,0,0,0.04)]"
       onClick={onClick}
     >
-      <div>{formatMonthDay(date)}</div>
+      <div>{date}</div>
       <CalendarBadge variation={category} />
       <div>{title}</div>
     </button>

1e3c439cf11b89dbf0b06206af23f29cea525bbf
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-12-28 13:22:38 +0900

    chore: 카카오 로그인 수정

--- src/features/auth/ui/KakaoLoginButton.tsx
+++ src/features/auth/ui/KakaoLoginButton.tsx
@@ -4,12 +4,12 @@ import { kakaoLogin } from '../lib/kakaoLogin';
 export function KakaoLoginButton() {
   return (
     <button
-      className="flex h-[3rem] w-full cursor-pointer items-center justify-center gap-[0.5rem] rounded-[0.5rem] bg-[#FEE500]"
+      className="rounded-4 flex h-[3rem] w-full cursor-pointer items-center justify-center gap-[0.5rem] bg-[#FEE500]"
       onClick={kakaoLogin}
     >
       {/* 카카오 아이콘으로 변경 필요 */}
-      <SurfIcon name="ChatSolid" size="l" className="text-logo-normal" />
-      <div className="text-logo-normal text-body-16-600--1">카카오로 로그인하기</div>
+      <SurfIcon name="ChatSolid" size="l" className="text-foreground-static-black" />
+      <div className="text-foreground-static-black text-title-title2">카카오로 로그인하기</div>
     </button>
   );
 }

e1d51e6b083020eff78ae71e296068a328938e55
Dahee Jang <jecy123@naver.com>
2025-12-28 13:37:20 +0900

    fix: AnnouncementBar 컴포넌트 shadow 적용

--- src/entities/schedule/ui/announcement-bar/AnnouncementBar.tsx
+++ src/entities/schedule/ui/announcement-bar/AnnouncementBar.tsx
@@ -12,7 +12,7 @@ interface AnnouncementBarProps {
 export const AnnouncementBar = ({ title, date, category, onClick }: AnnouncementBarProps) => {
   return (
     <button
-      className="text-body-body9 text-foreground-normal bg-background-normal-lighter rounded-5 flex h-[3.125rem] w-full items-center gap-10 px-13"
+      className="text-body-body9 text-foreground-normal bg-background-normal-lighter rounded-5 border-border-secondary flex h-[3.125rem] w-full items-center gap-10 border px-13 shadow-[0_0_20px_3px_rgba(0,0,0,0.04)]"
       onClick={onClick}
     >
       <div>{formatMonthDay(date)}</div>

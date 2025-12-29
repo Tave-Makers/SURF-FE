a6ddf7c6cbd054b3b4f28d16ee011a6c3cdd287b
jinj00oo <157488644+jinj00oo@users.noreply.github.com>
2025-12-29 22:29:57 +0900

    [FEAT] 검색 페이지 제작 및 API 연동 (#302)
    * feat: 경로 및 페이지 헤더 설정
    
    * chore: 회원 검색 도메인 타입 수정
    
    * chore: 페이지 메타 및 api 타입 정의
    
    * chore: 매퍼 정의
    
    * chore: university 필드 추가
    
    * chore: memberSearch api 요청 함수 작성
    
    * chore: useMemberSearch 훅 작성
    
    - 쿼리키 작성
    - 쿼리옵션 작성
    - 쿼리옵션에서 사용할 toMemberSearchRequest 매퍼 작성
    
    * chore: useMemberFilters 훅 제작
    
    * chore: 멤버 검색 위젯 제작
    
    * chore: 멤버 리스트 위젯 제작
    
    * feat: MemberSearchPage 제작
    
    * fix: BadgeIcon 조건부 렌더
    
    * chore: 검색어가 있으면 학교, 없으면 소개글을 보여주기
    
    * chore: 총 기수, 현재 기수 상수화
    
    * chore: 라우팅 todo 작성
    
    * feat: 검색어 디바운싱
    
    * chore: 리뷰 반영
    
    - 에러 처리
    - 임포트문 정리
    - 옵저버 언마운트시 정리
    
    * chore: API 에러 처리
    
    * chore: 파일 위치 이동
    
    * chore: 패딩 디자인 토큰 적용

---
+++ src/app/(protected)/(sub)/member-search/page.tsx
@@ -0,0 +1,5 @@
+import { MemberSearchPage } from '@/app-pages/member-search/ui/MemberSearchPage';
+
+export default function Page() {
+  return <MemberSearchPage />;
+}
---
+++ src/features/member-search/api/types.ts
@@ -0,0 +1,33 @@
+import { ServerUserLevel } from '@/entities/user/model/types';
+import { CommonResponse, PageMeta } from '@/shared/api/types';
+
+export interface MemberSearchRequestDTO {
+  pageNum: number;
+  pageSize: number;
+  keyword?: string;
+  generation?: number;
+  part?: string;
+}
+
+export interface MemberSearchItemDTO {
+  memberId: number;
+  username: string;
+  university: string;
+  selfIntroduction: string | null;
+  profileImageUrl: string | null;
+  role: ServerUserLevel;
+  trackList: {
+    generation: number;
+    part: string;
+  }[];
+}
+
+export interface MemberSearchPageMeta extends PageMeta {
+  totalCount: number | null;
+}
+
+export interface MemberSearchResponse extends MemberSearchPageMeta {
+  content: MemberSearchItemDTO[];
+}
+
+export type MemberSearchApiResponse = CommonResponse<MemberSearchResponse>;

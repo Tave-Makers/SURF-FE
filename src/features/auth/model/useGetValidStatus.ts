deba430ef7067a2eba212533f41778ed8798f529
rngrhn4114 <is9206855@gmail.com>
2026-01-03 22:27:00 +0900

    chore: 주석 삭제

--- src/features/auth/model/useGetValidStatus.ts
+++ src/features/auth/model/useGetValidStatus.ts
@@ -7,7 +7,7 @@ export function useGetValidStatus() {
   return useQuery({
     queryKey: GET_VALID_STATUS_KEY,
     queryFn: getValidStatus,
-    retry: false, // 로그인 체크 실패 시 재시도하지 않음
-    staleTime: 1000 * 60 * 5, // 5분간 캐시 유지 (필요에 따라 조절)
+    retry: false,
+    staleTime: 1000 * 60 * 5,
   });
 }

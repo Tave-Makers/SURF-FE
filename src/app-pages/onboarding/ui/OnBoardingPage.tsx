9418031d3d4546b4b7983c16447a1cac46e7ff7e
Dahee Jang <jecy123@naver.com>
2026-01-03 18:19:24 +0900

    chore: OnboardingPage에서 중복 kakaoImgNormalize 제거

--- src/app-pages/onboarding/ui/OnBoardingPage.tsx
+++ src/app-pages/onboarding/ui/OnBoardingPage.tsx
@@ -5,10 +5,9 @@ import { OnBoardingFormData } from '@/features/onboarding/model/types';
 import OnBoardingForm from '@/widgets/onboarding/ui/OnBoardingForm';
 import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
-import { kakaoImgNormalize } from '@/shared/lib/kakaoImgNormalize';
 
 export default function OnBoardingPage() {
-  const profileImageUrl = kakaoImgNormalize(useAuthStore((state) => state.profileImageUrl));
+  const profileImageUrl = useAuthStore((state) => state.profileImageUrl);
 
   const methods = useForm<OnBoardingFormData>({
     defaultValues: {

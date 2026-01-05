113bc3b2f402c4633334353205297d896f6d86f3
rngrhn4114 <is9206855@gmail.com>
2026-01-05 23:05:35 +0900

    fix: 일정 카테고리 default 값 regular로 전송하는거 수정

--- src/widgets/schedule/ui/ScheduleForm.tsx
+++ src/widgets/schedule/ui/ScheduleForm.tsx
@@ -28,7 +28,7 @@ export default function ScheduleForm({ onSubmit, initialData }: ScheduleFormProp
   useEffect(() => {
     if (initialData) {
       reset({
-        category: initialData.category || 'regular',
+        category: initialData.category || '',
         title: initialData.title ?? '',
         startDate: ensureUtcDate(initialData.startDate),
         endDate: ensureUtcDate(initialData.endDate),

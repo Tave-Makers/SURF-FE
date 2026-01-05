ef36ea661aed5f4ed496c05bb4270e609d730d2b
rngrhn4114 <is9206855@gmail.com>
2026-01-05 23:32:01 +0900

    fix: 게시글 상세 페이지 일정 요청 api 교체

--- src/app-pages/post/PostDetailPage.tsx
+++ src/app-pages/post/PostDetailPage.tsx
@@ -17,7 +17,7 @@ import { Alert } from '@/shared/ui/alert/Alert';
 import { usePathname, useRouter } from 'next/navigation';
 import { Avatar } from '@/shared/ui/avatar/Avatar';
 import { categoryIdToKey } from '@/entities/post/model/category';
-import { useGetSingleSchedule } from '@/features/schedule/edit/model/useGetSingleSchedule';
+import { useGetPostScheduleQuery } from '@/features/post/model/useGetPostScheduleQuery';
 import { useDeletePostMutation } from '@/features/post/model/useDeletePostMutation';
 import { useToastStore } from '@/shared/store/toastStore';
 
@@ -36,15 +36,11 @@ export default function PostDetailPage({ postId }: PostDetailPageProps) {
   const { data: post, isLoading, isError } = usePostDetail(numericPostId);
 
   // 일정 조회 API
-  const scheduleId = post?.scheduleId;
-
   const {
     data: schedule,
     isLoading: isScheduleLoading,
     isError: isScheduleError,
-  } = useGetSingleSchedule(scheduleId, {
-    enabled: !!scheduleId,
-  });
+  } = useGetPostScheduleQuery(numericPostId, !!post?.hasSchedule);
 
   // 좋아요 누른 사람 목록 API
   const {
@@ -61,7 +57,7 @@ export default function PostDetailPage({ postId }: PostDetailPageProps) {
   const { mutate: deletePostMutate } = useDeletePostMutation();
 
   // 로딩/에러 처리
-  if (isLoading || (scheduleId && isScheduleLoading))
+  if (isLoading || (post?.hasSchedule && isScheduleLoading))
     return (
       <div className="flex h-full w-full items-center justify-center">
         <span>불러오는 중...</span>
@@ -75,9 +71,9 @@ export default function PostDetailPage({ postId }: PostDetailPageProps) {
       </div>
     );
 
-  if (scheduleId && isScheduleError) {
+  if (post?.hasSchedule && isScheduleError) {
     if (process.env.NODE_ENV === 'development') {
-      console.warn(`일정 정보(ID: ${scheduleId})를 불러올 수 없습니다.`);
+      console.warn(`일정 정보(PostID: ${numericPostId})를 불러올 수 없습니다.`);
     }
   }
 

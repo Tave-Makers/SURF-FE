07140586c94261d6577838858c3c941bd6c8fe16
rngrhn4114 <is9206855@gmail.com>
2026-01-06 01:42:12 +0900

    feat: 테마 변경 화면 추가 구현

---
+++ src/app-pages/mypage/settings/mode/ui/ModePage.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { SettingsItem } from '@/entities/settings/ui/SettingsItem';
+import { HeaderMode } from '@/shared/ui/header/Header';
+import { Radio } from '@/shared/ui/radio/Radio';
+import { AppHeader } from '@/widgets/header/ui/AppHeader';
+import { THEME_OPTIONS } from '@/widgets/settings-list/model/constants';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export const ModePage = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <div>
+      <AppHeader
+        overrideHeader={{
+          title: '테마 변경',
+          mode: HeaderMode.Default,
+          hasLeftIcon: true,
+        }}
+      />
+
+      {THEME_OPTIONS.map((option) => (
+        <SettingsItem
+          key={option.value}
+          leftIconName={option.icon}
+          rightContent={
+            <div>
+              <Radio
+                id={option.value}
+                name="mode"
+                value={option.value}
+                isChecked={theme === option.value}
+                onChange={() => setTheme(option.value)}
+              />
+            </div>
+          }
+          onClick={() => setTheme(option.value)}
+        >
+          {option.label}
+        </SettingsItem>
+      ))}
+    </div>
+  );
+};

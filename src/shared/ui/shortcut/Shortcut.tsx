cebdcd6f66e88b96277f95f3e016c17794c82a02
jinj00oo <icems0428@naver.com>
2026-01-11 01:26:35 +0900

    chore: shortcut 스토리북 이미지 svg로 변경

--- src/shared/ui/shortcut/Shortcut.tsx
+++ src/shared/ui/shortcut/Shortcut.tsx
@@ -29,7 +29,7 @@ export const Shortcut = ({ type, label, imageSrc, onClick }: ShortcutProps) => {
       <div className="text-foreground-normal text-body-body5 px-13 pt-13">{label}</div>
 
       {/* 이미지 영역 */}
-      <div className="w-full flex-1">
+      <div className="w-full flex-1 bg-gray-200">
         {SvgIcon && <SvgIcon className="h-full w-full object-cover" />}
       </div>
     </button>

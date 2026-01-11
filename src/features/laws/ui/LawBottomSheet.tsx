4533bc8d519a6dfc4aed7a9d28a39f0b0f3a4e5c
rngrhn4114 <is9206855@gmail.com>
2026-01-11 14:21:13 +0900

    chore: className 추가

--- src/features/laws/ui/LawBottomSheet.tsx
+++ src/features/laws/ui/LawBottomSheet.tsx
@@ -57,7 +57,7 @@ export function LawBottomSheet({
           </Sheet>
         </ModalSheet.Content>
       </ModalSheet.Container>
-      <ModalSheet.Backdrop className="bg-effect-overlay-dim-normal" />
+      <ModalSheet.Backdrop className="bg-effect-overlay-dim-normal pointer-events-auto touch-none" />
     </ModalSheet>
   );
 }

94ce0d88abab03b3dc13d67bc264c6ae370ae299
jinj00oo <157488644+jinj00oo@users.noreply.github.com>
2026-01-11 16:55:23 +0900

    [CHORE] 홈화면 Shortcut 아이콘 및 Carousel 쉐도우 적용 (#378)
    * chore: shortcut 아이콘 저장
    
    * chore: shortcut 이미지에 svg 적용
    
    * chore: Shortcut 반응형으로 수정
    
    * chore: shadow 토큰 적용
    
    * chore: shortcut 스토리북 이미지 svg로 변경
    
    * chore: 이미지 최적화
    
    * chore: vitest.shim.d.ts에서 svg 타입 정의 삭제
    
    - svgr.d.ts에 정의되어 있음
    
    * chore: 리뷰 반영
    
    * chore: 접근성 개선
    
    * chore: 접근성 수정
    
    * chore: svg에서 object-cover 삭제
    
    * chore: 텍스트 토큰 적용

--- src/entities/home/model/constants.ts
+++ src/entities/home/model/constants.ts
@@ -1,40 +1,44 @@
 import { PAGE_ROUTES } from '@/shared/config/path';
+import NoticeIcon from '../ui/icons/notice.svg';
+import CalendarIcon from '../ui/icons/calendar.svg';
+import AddressBookIcon from '../ui/icons/address-book.svg';
 
 export interface ShortcutItem {
   id: number;
   label: string;
-  imageSrc: string;
+  imageSrc?: React.FC<React.SVGProps<SVGSVGElement>>;
   link: string; // 클릭 시 이동할 주소
 }
 
 // 1. 앱 내 바로가기 데이터
 export const SHORTCUT_LINKS: ShortcutItem[] = [
-  { id: 1, label: '공지사항', imageSrc: '/icons/notice.png', link: PAGE_ROUTES.BOARD.MAIN },
-  { id: 2, label: '일정', imageSrc: '/icons/notice.png', link: PAGE_ROUTES.CALENDAR.MAIN },
-  { id: 3, label: '주소록', imageSrc: '/icons/job.png', link: PAGE_ROUTES.MEMBER_SEARCH },
+  { id: 1, label: '공지사항', imageSrc: NoticeIcon, link: PAGE_ROUTES.BOARD.MAIN },
+  { id: 2, label: '일정', imageSrc: CalendarIcon, link: PAGE_ROUTES.CALENDAR.MAIN },
+  { id: 3, label: '주소록', imageSrc: AddressBookIcon, link: PAGE_ROUTES.MEMBER_SEARCH },
 ];
 
 // 2. TAVE 채널 데이터
+// TODO: svg 이미지 추가 필요
 export const TAVE_CHANNEL_LINKS: ShortcutItem[] = [
   {
     id: 1,
     label: '인스타그램',
-    imageSrc: '/icons/instagram.png',
     link: 'https://instagram.com/...',
   },
-  { id: 2, label: '유튜브', imageSrc: '/icons/youtube.png', link: 'https://youtube.com/...' },
-  { id: 3, label: '미디엄', imageSrc: '/icons/medium.png', link: 'https://medium.com/...' },
-  { id: 4, label: '공식 홈페이지', imageSrc: '/icons/homepage.png', link: 'https://tave.or.kr' },
+  { id: 2, label: '유튜브', link: 'https://youtube.com/...' },
+  { id: 3, label: '미디엄', link: 'https://medium.com/...' },
+  { id: 4, label: '공식 홈페이지', link: 'https://tave.or.kr' },
 ];
 
 // 3. 후원사 데이터
+// TODO: svg 이미지 추가 필요
 export const SPONSOR_LINKS: ShortcutItem[] = [
-  { id: 1, label: '후원사 A', imageSrc: '/sponsors/sponsor-a.png', link: 'https://...' },
-  { id: 2, label: '후원사 B', imageSrc: '/sponsors/sponsor-b.png', link: 'https://...' },
-  { id: 3, label: '후원사 C', imageSrc: '/sponsors/sponsor-c.png', link: 'https://...' },
-  { id: 4, label: '후원사 D', imageSrc: '/sponsors/sponsor-d.png', link: 'https://...' },
-  { id: 5, label: '후원사 E', imageSrc: '/sponsors/sponsor-e.png', link: 'https://...' },
-  { id: 6, label: '후원사 F', imageSrc: '/sponsors/sponsor-f.png', link: 'https://...' },
-  { id: 7, label: '후원사 G', imageSrc: '/sponsors/sponsor-g.png', link: 'https://...' },
-  { id: 8, label: '후원사 H', imageSrc: '/sponsors/sponsor-h.png', link: 'https://...' },
+  { id: 1, label: '후원사 A', link: 'https://...' },
+  { id: 2, label: '후원사 B', link: 'https://...' },
+  { id: 3, label: '후원사 C', link: 'https://...' },
+  { id: 4, label: '후원사 D', link: 'https://...' },
+  { id: 5, label: '후원사 E', link: 'https://...' },
+  { id: 6, label: '후원사 F', link: 'https://...' },
+  { id: 7, label: '후원사 G', link: 'https://...' },
+  { id: 8, label: '후원사 H', link: 'https://...' },
 ];

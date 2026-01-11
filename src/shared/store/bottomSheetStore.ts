021abc7e2c646f53f7797eb7c9b340d4cfa498ef
rngrhn4114 <is9206855@gmail.com>
2026-01-11 15:46:02 +0900

    chore: 주석 설명 추가

--- src/shared/store/bottomSheetStore.ts
+++ src/shared/store/bottomSheetStore.ts
@@ -1,5 +1,20 @@
 import { create } from 'zustand';
 
+/**
+ * Bottom Sheet System Guide
+ * 1. 새로운 바텀 시트 등록하기
+ * - 새로운 종류의 바텀 시트를 만들고 싶다면, 먼저 타입을 등록
+ * 예) lawBottomSheetSchema.ts와 같이
+ * src/features/xxx/model/schema.ts같은 곳에서 기존에 구현된 바텀 시트 props의 타입을 가져와서 선언
+ *
+ * 2. 바텀 시트 컴포넌트 구현
+ * 3. 바텀 시트를 사용하는 페이지에서 useBottomSheetStore 를 사용하여 시트를 열고 닫기
+ *    - const open = useBottomSheetStore((s) => s.open);
+ *    - const close = useBottomSheetStore((s) => s.close);
+ *
+ * 4. props는 해당 type(키)에 맞는 props를 작성(타입 체크 됨)
+ */
+
 // Registry Pattern: Features will augment this interface
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface BottomSheetMap {}

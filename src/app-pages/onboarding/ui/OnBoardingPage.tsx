849fab4ba12cdec5f538fd785e36c1ec8a3fec3e
rngrhn4114 <is9206855@gmail.com>
2026-01-11 16:42:51 +0900

    chore: 주석 해제

--- src/app-pages/onboarding/ui/OnBoardingPage.tsx
+++ src/app-pages/onboarding/ui/OnBoardingPage.tsx
@@ -30,9 +30,7 @@ export default function OnBoardingPage() {
   const closeBottomSheet = useBottomSheetStore((s) => s.close);
 
   useEffect(() => {
-    const shouldOpen =
-      // statusData?.memberStatus === 'REGISTERING' &&
-      !isAgreed && step != 2;
+    const shouldOpen = statusData?.memberStatus === 'REGISTERING' && !isAgreed && step != 2;
 
     if (shouldOpen) {
       openBottomSheet({

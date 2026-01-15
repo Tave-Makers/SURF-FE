00f7f0eb87f17e1052f7943c67603956e89db55b
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-01-15 21:45:20 +0900

    fix: memberId 유효성 검사

--- apps/web/src/app/(protected)/(sub)/member/[id]/page.tsx
+++ apps/web/src/app/(protected)/(sub)/member/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import { MemberProfilePage } from '@/app-pages/member/ui/MemberProfilePage';
 import { getProfile } from '@/entities/user/api/getProfile.server';
 import { mapUserProfile } from '@/entities/user/model/mappers';
@@ -9,6 +10,9 @@ interface Props {
 const Page = async ({ params }: Props) => {
   const { id } = await params;
   const memberId = Number(id);
+  if (Number.isNaN(memberId) || memberId <= 0) {
+    notFound();
+  }
   const res = await getProfile({ memberId });
   const profile = mapUserProfile(res.data);
 

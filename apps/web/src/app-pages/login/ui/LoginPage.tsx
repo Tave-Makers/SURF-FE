b8149b1090696423c1303c60de98487648e12c20
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-01-15 11:20:09 +0900

    feat: 비회원 로그인 클릭 시 모달 출력

--- apps/web/src/app-pages/login/ui/LoginPage.tsx
+++ apps/web/src/app-pages/login/ui/LoginPage.tsx
@@ -1,9 +1,44 @@
 'use client';
 
+import { useAlertStore } from '@surf/ui/store/alertStore';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 import Logo from '../../../../public/logo.svg';
 import { KakaoLoginButton } from '@/features/auth/ui/KakaoLoginButton';
 
+import { PAGE_ROUTES } from '@/shared/config/path';
+
 export const LoginPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const openAlert = useAlertStore((s) => s.open);
+  const closeAlert = useAlertStore((s) => s.close);
+
+  const msg = searchParams.get('msg');
+
+  useEffect(() => {
+    if (!msg) return;
+
+    if (msg === 'pending') {
+      router.replace(PAGE_ROUTES.LOGIN);
+      openAlert({
+        state: 'default',
+        title: '회원가입 대기중이에요!',
+        infoText: '회원 승인 절차가 완료되면 정상적으로 SURF를 이용하실 수 있습니다.',
+        actions: [
+          {
+            type: 'text',
+            variant: 'primary',
+            label: '확인',
+            onClick: () => {
+              closeAlert();
+            },
+          },
+        ],
+      });
+    }
+  }, [msg, openAlert, closeAlert, router]);
+
   return (
     <div className="flex h-dvh w-dvw flex-col items-center gap-[6.75rem] px-15 pt-[16.81rem]">
       <Logo width={163.684} height={55.04} />
--- apps/web/src/app/(public)/login/page.tsx
+++ apps/web/src/app/(public)/login/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from 'react';
 import { LoginPage } from '@/app-pages/login/ui/LoginPage';
-const Page = () => {
-  return <LoginPage />;
-};
 
+const Page = () => {
+  return (
+    <Suspense fallback={null}>
+      <LoginPage />
+    </Suspense>
+  );
+};
 export default Page;

5e1c64d5dd1c79b5b11b3b038295841ace785be5
ej070961 <ej070961@gmail.com>
2026-01-18 23:56:13 +0900

    refactor: 로고 접근성 개선(#417)

--- apps/admin/src/app-pages/login/ui/LoginPage.tsx
+++ apps/admin/src/app-pages/login/ui/LoginPage.tsx
@@ -5,7 +5,7 @@ export const LoginPage = () => {
   return (
     <div className="flex h-dvh w-dvw flex-col px-16">
       <div className="relative flex w-full grow flex-col items-center justify-center gap-[2.25rem]">
-        <Logo width={224.61} height={115.8} />
+        <Logo width={224.61} height={115.8} role="img" aria-label="SURF 어드민 로고" />
         <LoginForm />
       </div>
     </div>

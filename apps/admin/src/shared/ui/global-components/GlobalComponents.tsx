f5b456bb5f59030fd693fd85d5dac51c1feda695
ej070961 <ej070961@gmail.com>
2026-01-18 17:53:09 +0900

    feat: 전역 레이아웃에 toast, alert 추가

---
+++ apps/admin/src/shared/ui/global-components/GlobalComponents.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { AlertViewport } from '@surf/ui/alert';
+import { ToastViewport } from '@surf/ui/toast';
+
+export const GlobalComponents = () => {
+  return (
+    <>
+      <ToastViewport />
+      <AlertViewport />
+    </>
+  );
+};

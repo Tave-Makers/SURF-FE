766ed743279c6b0c5898abf6bf954ec25ac31eda
ej070961 <ej070961@gmail.com>
2026-01-21 21:16:03 +0900

    feat: 어드민 홈 화면 구현(#457)

--- apps/admin/src/app-pages/home/ui/HomePage.tsx
+++ apps/admin/src/app-pages/home/ui/HomePage.tsx
@@ -1,9 +1,18 @@
+import { HeaderMode } from '@surf/ui/header';
 import { AppHeader } from '@/widgets/header/ui/AppHeader';
+import { AppNavigation } from '@/widgets/navigation/ui/AppNavigation';
+import HeaderLogo from 'public/admin-header-logo.svg';
 
 export const HomePage = () => {
   return (
-    <div className="text-title-title2 text-foreground-normal">
-      <AppHeader />
+    <div className="w-full flex-1 flex-col">
+      <AppHeader
+        overrideHeader={{
+          mode: HeaderMode.Logo,
+          logo: <HeaderLogo />,
+        }}
+      />
+      <AppNavigation />
     </div>
   );
 };

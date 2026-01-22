0e9241f4b8aa1d0834db06fd3f26cefe1073532c
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-01-22 21:43:38 +0900

    feat: 홈 숏컷에 사진 및 링크 추가

--- apps/web/src/entities/home/model/constants.ts
+++ apps/web/src/entities/home/model/constants.ts
@@ -1,20 +1,22 @@
 import { PAGE_ROUTES } from '@/shared/config/path';
-import NoticeIcon from '@/shared/assets/icons/home/notice.svg';
-import CalendarIcon from '@/shared/assets/icons/home/calendar.svg';
-import AddressBookIcon from '@/shared/assets/icons/home/address-book.svg';
 
 export interface ShortcutItem {
   id: number;
   label: string;
-  imageSrc?: React.FC<React.SVGProps<SVGSVGElement>>;
+  imageSrc?: string;
   link: string; // 클릭 시 이동할 주소
 }
 
 // 1. 앱 내 바로가기 데이터
 export const SHORTCUT_LINKS: ShortcutItem[] = [
-  { id: 1, label: '공지사항', imageSrc: NoticeIcon, link: PAGE_ROUTES.BOARD.MAIN },
-  { id: 2, label: '일정', imageSrc: CalendarIcon, link: PAGE_ROUTES.CALENDAR.MAIN },
-  { id: 3, label: '주소록', imageSrc: AddressBookIcon, link: PAGE_ROUTES.MEMBER.MEMBER_SEARCH },
+  { id: 1, label: '공지사항', imageSrc: '/images/home/notice.svg', link: PAGE_ROUTES.BOARD.MAIN },
+  { id: 2, label: '일정', imageSrc: '/images/home/calendar.svg', link: PAGE_ROUTES.CALENDAR.MAIN },
+  {
+    id: 3,
+    label: '주소록',
+    imageSrc: '/images/home/address-book.svg',
+    link: PAGE_ROUTES.MEMBER.MEMBER_SEARCH,
+  },
 ];
 
 // 2. TAVE 채널 데이터
@@ -22,23 +24,19 @@ export const SHORTCUT_LINKS: ShortcutItem[] = [
 export const TAVE_CHANNEL_LINKS: ShortcutItem[] = [
   {
     id: 1,
-    label: '인스타그램',
+    label: '공식 홈페이지',
     link: 'https://instagram.com/...',
   },
-  { id: 2, label: '유튜브', link: 'https://youtube.com/...' },
+  { id: 2, label: '인스타그램', link: 'https://youtube.com/...' },
   { id: 3, label: '미디엄', link: 'https://medium.com/...' },
   { id: 4, label: '공식 홈페이지', link: 'https://tave.or.kr' },
 ];
 
 // 3. 후원사 데이터
-// TODO: svg 이미지 추가 필요
 export const SPONSOR_LINKS: ShortcutItem[] = [
-  { id: 1, label: '후원사 A', link: 'https://...' },
-  { id: 2, label: '후원사 B', link: 'https://...' },
-  { id: 3, label: '후원사 C', link: 'https://...' },
-  { id: 4, label: '후원사 D', link: 'https://...' },
-  { id: 5, label: '후원사 E', link: 'https://...' },
-  { id: 6, label: '후원사 F', link: 'https://...' },
-  { id: 7, label: '후원사 G', link: 'https://...' },
-  { id: 8, label: '후원사 H', link: 'https://...' },
+  { id: 1, label: 'F-Lab', link: 'https://f-lab.kr/', imageSrc: '/images/tave-channel/agit.webp' },
+  { id: 2, label: '렛츠커리어', link: 'https://www.letscareer.co.kr/' },
+  { id: 3, label: '알파코', link: 'https://corp.alpaco.co.kr/' },
+  { id: 4, label: 'Upstage', link: 'https://www.upstage.ai/' },
+  { id: 5, label: '코드잇', link: 'https://www.codeit.kr/promotions/year-end/' },
 ];

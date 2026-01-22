4d796faaa8c00253ca101c58671e4ea9e07b7705
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-01-22 19:21:43 +0900

    feat: 중복 방지 및 거절 시 alert 추가

--- apps/web/src/app-pages/login/ui/LoginPage.tsx
+++ apps/web/src/app-pages/login/ui/LoginPage.tsx
@@ -2,7 +2,7 @@
 
 import { useAlertStore } from '@surf/ui/store/alertStore';
 import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Logo from '../../../../public/logo.svg';
 import { KakaoLoginButton } from '@/features/auth/ui/KakaoLoginButton';
 
@@ -10,17 +10,39 @@ export const LoginPage = () => {
   const searchParams = useSearchParams();
   const openAlert = useAlertStore((s) => s.open);
   const closeAlert = useAlertStore((s) => s.close);
+  const alertOpenedRef = useRef(false);
 
   const msg = searchParams.get('msg');
 
   useEffect(() => {
     if (!msg) return;
+    if (alertOpenedRef.current) return;
 
     if (msg === 'pending') {
+      alertOpenedRef.current = true;
       openAlert({
         state: 'default',
         title: '회원가입 대기중이에요!',
         infoText: '회원 승인 절차가 완료되면 정상적으로 SURF를 이용하실 수 있습니다.',
+        actions: [
+          {
+            type: 'text',
+            variant: 'primary',
+            label: '확인',
+            onClick: () => {
+              closeAlert();
+            },
+          },
+        ],
+      });
+    }
+
+    if (msg === 'rejected') {
+      alertOpenedRef.current = true;
+      openAlert({
+        state: 'default',
+        title: '회원가입이 거절됐어요.',
+        infoText: '다시 회원가입을 원하시면 tavemakers@gmail.com으로 연락주세요.',
         actions: [
           {
             type: 'text',

540befb545fe85057baf4a2ecd2f5663ef29f4c5
Jiyeon <144954836+Chaejy@users.noreply.github.com>
2026-01-22 22:10:17 +0900

    fix: constants 수정

--- apps/web/src/entities/home/model/constants.ts
+++ apps/web/src/entities/home/model/constants.ts
@@ -4,39 +4,29 @@ export interface ShortcutItem {
   id: number;
   label: string;
   imageSrc?: string;
-  link: string; // 클릭 시 이동할 주소
+  link: string;
 }
 
-// 1. 앱 내 바로가기 데이터
+// 앱 내 바로가기
 export const SHORTCUT_LINKS: ShortcutItem[] = [
   { id: 1, label: '공지사항', imageSrc: '/images/home/notice.svg', link: PAGE_ROUTES.BOARD.MAIN },
   { id: 2, label: '일정', imageSrc: '/images/home/calendar.svg', link: PAGE_ROUTES.CALENDAR.MAIN },
-  {
-    id: 3,
-    label: '주소록',
-    imageSrc: '/images/home/address-book.svg',
-    link: PAGE_ROUTES.MEMBER.MEMBER_SEARCH,
-  },
+  { id: 3, label: '주소록', imageSrc: '/images/home/address-book.svg', link: PAGE_ROUTES.MEMBER.MEMBER_SEARCH },
 ];
 
-// 2. TAVE 채널 데이터
-// TODO: svg 이미지 추가 필요
+// 테이브 채널
 export const TAVE_CHANNEL_LINKS: ShortcutItem[] = [
-  {
-    id: 1,
-    label: '공식 홈페이지',
-    link: 'https://instagram.com/...',
-  },
-  { id: 2, label: '인스타그램', link: 'https://youtube.com/...' },
-  { id: 3, label: '미디엄', link: 'https://medium.com/...' },
-  { id: 4, label: '공식 홈페이지', link: 'https://tave.or.kr' },
+  { id: 1, label: 'Homepage', link: 'https://www.tave-wave.com/', imageSrc: '/images/tave-channel/homepage.svg' },
+  { id: 2, label: 'Agit', link: 'https://tave.agit.io/home/', imageSrc: '/images/tave-channel/agit.webp' },
+  { id: 3, label: 'Instagram', link: 'https://www.instagram.com/tave_wave/', imageSrc: '/images/tave-channel/instagram.webp' },
+  { id: 4, label: 'Blog', link: 'https://blog.naver.com/t-ave/', imageSrc: '/images/tave-channel/blog.webp' },
 ];
 
-// 3. 후원사 데이터
+// 후원사
 export const SPONSOR_LINKS: ShortcutItem[] = [
-  { id: 1, label: 'F-Lab', link: 'https://f-lab.kr/', imageSrc: '/images/tave-channel/agit.webp' },
-  { id: 2, label: '렛츠커리어', link: 'https://www.letscareer.co.kr/' },
-  { id: 3, label: '알파코', link: 'https://corp.alpaco.co.kr/' },
-  { id: 4, label: 'Upstage', link: 'https://www.upstage.ai/' },
-  { id: 5, label: '코드잇', link: 'https://www.codeit.kr/promotions/year-end/' },
+  { id: 1, label: 'F-Lab', link: 'https://f-lab.kr/', imageSrc: '/images/sponsor/f-lab.jpg' },
+  { id: 2, label: '렛츠커리어', link: 'https://www.letscareer.co.kr/', imageSrc: '/images/sponsor/letscareer.webp' },
+  { id: 3, label: '알파코', link: 'https://corp.alpaco.co.kr/', imageSrc: '/images/sponsor/alpaco.png' },
+  { id: 4, label: 'Upstage', link: 'https://www.upstage.ai/', imageSrc: '/images/sponsor/upstage.png' },
+  { id: 5, label: '코드잇', link: 'https://www.codeit.kr/promotions/year-end/', imageSrc: '/images/sponsor/codeit.png' },
 ];

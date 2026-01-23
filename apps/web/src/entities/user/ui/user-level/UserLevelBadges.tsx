257d2b67c4bb43afb34794a6745faa8adbae28cc
jinj00oo <icems0428@naver.com>
2026-01-23 16:23:53 +0900

    fix: 멤버 뱃지 삭제

--- apps/web/src/entities/user/ui/user-level/UserLevelBadges.tsx
+++ apps/web/src/entities/user/ui/user-level/UserLevelBadges.tsx
@@ -4,11 +4,10 @@ import type { UserLevel } from '@/entities/user/model/types';
 import ManagerBadge from '@/shared/assets/icons/profile/manager-badge.svg';
 import AdminBadge from '@/shared/assets/icons/profile/manager-badge.svg';
 import PresidentBadge from '@/shared/assets/icons/profile/manager-badge.svg';
-import MemberBadge from '@/shared/assets/icons/profile/manager-badge.svg';
 
-export const USER_LEVEL_BADGE: Record<UserLevel, ComponentType<SVGProps<SVGSVGElement>>> = {
+export const USER_LEVEL_BADGE: Record<UserLevel, ComponentType<SVGProps<SVGSVGElement>> | null> = {
   admin: AdminBadge,
   president: PresidentBadge,
   manager: ManagerBadge,
-  member: MemberBadge,
+  member: null,
 };

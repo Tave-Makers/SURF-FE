ff9e2aaf9f4cececa83cafca14ed9517905f54af
ej070961 <ej070961@gmail.com>
2026-02-03 21:35:38 +0900

    feat: 테스트 코드 추가(#496)

--- apps/admin/src/test/mocks/server.ts
+++ apps/admin/src/test/mocks/server.ts
@@ -1,4 +1,4 @@
-import { setupServer } from 'msw/node';
+import { setupServer, type SetupServerApi } from 'msw/node';
 import { signupRequestHandlers } from './handlers/signupRequest';
 
-export const server = setupServer(...signupRequestHandlers);
+export const server: SetupServerApi = setupServer(...signupRequestHandlers);

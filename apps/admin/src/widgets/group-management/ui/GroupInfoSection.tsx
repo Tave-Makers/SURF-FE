d2568d32f28f2f2c8b58437396f1caf1613aca7d
jinj00oo <icems0428@naver.com>
2026-02-17 21:55:19 +0900

    feat: GroupInfoSection 위젯 제작

---
+++ apps/admin/src/widgets/group-management/ui/GroupInfoSection.tsx
@@ -0,0 +1,72 @@
+import { FieldGroup } from '@surf/ui/field-group';
+import { SelectField } from '@surf/ui/select-field';
+import { TextArea } from '@surf/ui/text-area';
+import { ContentsType } from '@/shared/types/contents';
+import { GroupManagementMode } from '@/widgets/group-management/model/types';
+
+interface GroupInfoSectionProps {
+  mode: GroupManagementMode;
+  generation: number;
+  groupType: ContentsType;
+  groupName: string;
+  groupIntroduction: string;
+  onOpenGeneration: () => void;
+  onOpenGroupType: () => void;
+  onChangeGroupName: (value: string) => void;
+  onChangeGroupIntroduction: (value: string) => void;
+}
+
+export const GroupInfoSection = ({
+  mode,
+  generation,
+  groupType,
+  groupName,
+  groupIntroduction,
+  onOpenGeneration,
+  onOpenGroupType,
+  onChangeGroupName,
+  onChangeGroupIntroduction,
+}: GroupInfoSectionProps) => {
+  const isDisabled = mode === 'view';
+  return (
+    <div className="flex flex-col gap-14 px-14">
+      <FieldGroup title="기수" isRequired>
+        <SelectField
+          size="l"
+          selectedValue={generation.toString() + '기'}
+          isDisabled={isDisabled}
+          onClick={onOpenGeneration}
+        />
+      </FieldGroup>
+      <FieldGroup title="그룹 유형" isRequired>
+        <SelectField
+          size="l"
+          selectedValue={groupType === 'study' ? '스터디' : '프로젝트'}
+          isDisabled={isDisabled}
+          onClick={onOpenGroupType}
+        />
+      </FieldGroup>
+      <FieldGroup title="그룹명" isRequired>
+        <TextArea
+          value={groupName}
+          mode="oneLine"
+          placeholder="그룹명을 입력해주세요."
+          readOnly={isDisabled}
+          isDisabled={isDisabled}
+          onChange={onChangeGroupName}
+        />
+      </FieldGroup>
+      <FieldGroup title="그룹 소개" isRequired>
+        <TextArea
+          value={groupIntroduction}
+          mode="multiLine"
+          textLimit={500}
+          placeholder="그룹 소개를 작성해주세요."
+          readOnly={isDisabled}
+          isDisabled={isDisabled}
+          onChange={onChangeGroupIntroduction}
+        />
+      </FieldGroup>
+    </div>
+  );
+};

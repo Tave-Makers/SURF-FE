7273980a4a0ed0c393ce6533cc7de0fca837a3c1
jinj00oo <icems0428@naver.com>
2026-02-17 21:55:32 +0900

    feat: 그룹 관리 페이지에 GroupInfoSection 연동

--- apps/admin/src/app-pages/group-management/ui/GroupManagementDetailPage.tsx
+++ apps/admin/src/app-pages/group-management/ui/GroupManagementDetailPage.tsx
@@ -2,13 +2,16 @@
 
 import { SolidButton } from '@surf/ui/button';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useState } from 'react';
 import {
   mapModeToStickyButton,
   mapModeToHeaderProps,
 } from '@/app-pages/group-management/model/mapper';
 import { mockLeader, mockMembers } from '@/app-pages/group-management/model/mock';
 import { useGroupMembersField } from '@/features/group-management/model/useGroupMembersField';
+import { ContentsType } from '@/shared/types/contents';
 import { GroupManagementMode } from '@/widgets/group-management/model/types';
+import { GroupInfoSection } from '@/widgets/group-management/ui/GroupInfoSection';
 import { GroupMemberSection } from '@/widgets/group-management/ui/GroupMemberSection';
 import { AppHeader } from '@/widgets/header/ui/AppHeader';
 
@@ -27,13 +30,26 @@ export const GroupManagementDetailPage = ({ mode }: GroupManagementDetailPagePro
   };
   const headerProps = mapModeToHeaderProps({ mode, onClickEdit: handleSwitchToEdit });
 
+  const [generation, _setGeneration] = useState<number>(17); // TODO: 가장 최근 기수 API로 초기값 설정
+  const [groupType, _setGroupType] = useState<ContentsType>('study');
+  const [groupName, setGroupName] = useState('');
+  const [groupIntroduction, setGroupIntroduction] = useState('');
+
   // TODO : API 연동 후 mockData 제거
   const { leader, members, removeMember } = useGroupMembersField({
     leader: mockLeader,
     members: mockMembers,
   });
 
-  const handlePickLeader = () => {
+  const openGenerationBottomSheet = () => {
+    // 기수 선택 바텀시트 오픈
+  };
+
+  const openGroupTypeBottomSheet = () => {
+    // 그룹 유형 선택 바텀시트 오픈
+  };
+
+  const openPickLeaderBottomSheet = () => {
     // 오픈 바텀 시트
     // 선택 완료 시 pickLeader 호출
   };
@@ -50,17 +66,28 @@ export const GroupManagementDetailPage = ({ mode }: GroupManagementDetailPagePro
   return (
     <div className="flex h-full flex-col">
       <AppHeader overrideHeader={headerProps} />
-      <div className="scrollbar-hide flex flex-1 flex-col gap-20 overflow-y-auto">
+      <div className="scrollbar-hide flex flex-1 flex-col gap-14 overflow-y-auto">
+        <GroupInfoSection
+          mode={mode}
+          generation={generation}
+          groupType={groupType}
+          groupName={groupName}
+          groupIntroduction={groupIntroduction}
+          onOpenGeneration={openGenerationBottomSheet}
+          onOpenGroupType={openGroupTypeBottomSheet}
+          onChangeGroupName={setGroupName}
+          onChangeGroupIntroduction={setGroupIntroduction}
+        />
         <GroupMemberSection
           mode={mode}
           teamLeader={leader}
           teamMembers={members}
-          onPickLeader={handlePickLeader}
+          onPickLeader={openPickLeaderBottomSheet}
           onAddMembers={handleAddMembers}
           onRemoveMember={handleRemoveMember}
         />
         {mode === 'edit' && (
-          <div className="px-13 pb-15">
+          <div className="px-13 py-15">
             <SolidButton size="m" variant="warning" onClick={() => {}} className="shrink-0">
               해당 그룹 삭제하기
             </SolidButton>

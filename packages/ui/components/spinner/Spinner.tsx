8c2a5b301c93764587da093c400d2062fde4124d
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-04-19 10:59:14 +0900

    feat: spinner 컴포넌트 제작

---
+++ packages/ui/components/spinner/Spinner.tsx
@@ -0,0 +1,20 @@
+import SpinnerSvg from '../../assets/loading/spinner.svg';
+
+type SpinnerSize = 'xs' | 's' | 'm' | 'l' | 'xl';
+
+const sizeStyle: Record<SpinnerSize, string> = {
+  xs: 'w-4 h-4',
+  s: 'w-6 h-6',
+  m: 'w-8 h-8',
+  l: 'w-12 h-12',
+  xl: 'w-16 h-16',
+};
+
+type SpinnerProps = {
+  size?: SpinnerSize;
+  className?: string;
+};
+
+export const Spinner = ({ size = 'm', className = '' }: SpinnerProps) => {
+  return <SpinnerSvg className={`${sizeStyle[size]} ${className}`} />;
+};

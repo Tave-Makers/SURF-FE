f502b0caa92f3bbf9a238c47e297e74925aa3620
ej070961 <ej070961@gmail.com>
2026-07-26 18:59:36 +0900

    feat: Comment 컴포넌트 UI 개선

--- apps/web/src/features/comment/ui/Comment.tsx
+++ apps/web/src/features/comment/ui/Comment.tsx
@@ -91,7 +91,7 @@ export const Comment = ({
             </p>
           </div>
           <button type="button" aria-label="댓글 더보기" className="ml-auto" onClick={onMoreClick}>
-            <SurfIcon name="Dots" size="m" />
+            <SurfIcon name="Dots" size="l" />
           </button>
         </header>
 
@@ -104,7 +104,7 @@ export const Comment = ({
         <footer className="flex gap-11">
           <button
             type="button"
-            className="text-caption-caption4 text-foreground-secondary-lighter flex items-center gap-5"
+            className="text-caption-caption4 text-foreground-secondary-lighter flex items-center gap-4 p-4"
             aria-label={`좋아요 ${likeCount}개`}
             onClick={handleLikeToggle}
           >
@@ -121,7 +121,7 @@ export const Comment = ({
 
           <button
             type="button"
-            className="text-caption-caption4 text-foreground-secondary-lighter flex items-center gap-5"
+            className="text-caption-caption4 text-foreground-secondary-lighter flex items-center gap-4 p-4"
             aria-label="답글 달기"
             onClick={onReplyClick}
           >

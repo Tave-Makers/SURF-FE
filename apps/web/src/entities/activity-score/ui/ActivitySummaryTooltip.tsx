7556d5b9f40c41fdd8cf0ff877e97e9e87b573e1
Jiyeon <xhdrp@sookmyung.ac.kr>
2026-08-05 11:35:17 +0900

    chore: 툴팁 배경 디자인 토큰 변경

--- apps/web/src/entities/activity-score/ui/ActivitySummaryTooltip.tsx
+++ apps/web/src/entities/activity-score/ui/ActivitySummaryTooltip.tsx
@@ -8,7 +8,7 @@ type ActivitySummaryTooltipProps = {
 
 export const ActivitySummaryTooltip: FC<ActivitySummaryTooltipProps> = ({ activities }) => {
   return (
-    <div className="bg-background-normal rounded-3 flex w-full flex-col items-start px-10 py-7 shadow-[0_0_10px_0_rgba(0,0,0,0.08)]">
+    <div className="bg-background-normal-lighter rounded-3 flex w-full flex-col items-start px-10 py-7 shadow-[0_0_10px_0_rgba(0,0,0,0.08)]">
       {activities.map((a, i) => (
         <div key={i} className="text-caption-caption6 text-foreground-normal">
           {a.label} {a.count}회
